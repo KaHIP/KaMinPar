ddb28b91f51c849e248ccf91f8cbec94a884f6a2
Daniel Seemaier <daniel@seemaier.de>
2021-09-07 12:29:38 +0200

    Add skeleton for distributed tests.

--- cmake/modules/gmock.cmake
+++ cmake/modules/gmock.cmake
@@ -1,7 +1,7 @@
 # taken from http://johnlamp.net/cmake-tutorial-5-functionally-improved-testing.html
 function(add_gmock_test target)
     add_executable(${target} ${ARGN})
-    target_link_libraries(${target} gtest gtest_main gmock_main ${CMAKE_THREAD_LIBS_INIT} kaminpar_impl)
+    target_link_libraries(${target} PRIVATE gtest gtest_main gmock_main ${CMAKE_THREAD_LIBS_INIT})
 
     set_property(TARGET ${target} PROPERTY CXX_STANDARD 20)
     set_property(TARGET ${target} PROPERTY CXX_STANDARD_REQUIRED ON)
