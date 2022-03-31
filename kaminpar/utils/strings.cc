02a55ce1d346e6808573011419e9f2d59f9037d2
Daniel Seemaier <daniel.seemaier@kit.edu>
2022-03-31 18:15:19 +0200

    Refactor string utils test

--- kaminpar/utils/strings.cc
+++ kaminpar/utils/strings.cc
@@ -34,14 +34,17 @@ std::vector<std::string> explode(const std::string& str, const char del) {
     }
     return splits;
 }
+
 std::string& rtrim(std::string& s, const char* t) {
     s.erase(s.find_last_not_of(t) + 1);
     return s;
 }
+
 std::string& ltrim(std::string& s, const char* t) {
     s.erase(0, s.find_first_not_of(t));
     return s;
 }
+
 std::string& trim(std::string& s, const char* t) {
     return ltrim(rtrim(s, t), t);
 }
