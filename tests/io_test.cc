8bc4604ae4acec6c793ad36f47bcc757cbf704d8
Daniel Seemaier <daniel.seemaier@kit.edu>
2022-03-31 18:08:31 +0200

    Modernize tests

--- tests/io_test.cc
+++ tests/io_test.cc
@@ -1,12 +1,13 @@
-#include "gmock/gmock.h"
+#include <gmock/gmock.h>
+
 #include "kaminpar/datastructure/graph.h"
 #include "kaminpar/io.h"
 #include "tests.h"
 
 using ::testing::Eq;
 using ::testing::UnorderedElementsAre;
+using namespace kaminpar;
 
-namespace kaminpar::io {
 inline void assert_K3_structure(const Graph& G) {
     ASSERT_THAT(G.n(), Eq(3));
     ASSERT_THAT(G.m(), Eq(6));
@@ -23,8 +24,8 @@ inline auto outgoing_edge_weights(const Graph& G, const NodeID u) {
     return edge_weights;
 }
 
-TEST(IOTest, LoadsUnweightedK3Correctly) {
-    const auto G = metis::read(test::test_instance("unweighted_K3.graph"));
+TEST(IOTest, unweighted_K3) {
+    const auto G = io::metis::read(test::test_instance("unweighted_K3.graph"));
     assert_K3_structure(G);
 
     for (const NodeID& u: G.nodes())
@@ -33,8 +34,8 @@ TEST(IOTest, LoadsUnweightedK3Correctly) {
         ASSERT_THAT(G.edge_weight(e), Eq(1));
 }
 
-TEST(IOTest, LoadsNodeWeightedK3Correctly) {
-    const auto G = metis::read(test::test_instance("node_weighted_K3.graph"));
+TEST(IOTest, node_weighted_K3) {
+    const auto G = io::metis::read(test::test_instance("node_weighted_K3.graph"));
     assert_K3_structure(G);
 
     ASSERT_THAT(G.node_weight(0), Eq(1));
@@ -44,8 +45,8 @@ TEST(IOTest, LoadsNodeWeightedK3Correctly) {
         ASSERT_THAT(G.edge_weight(e), Eq(1));
 }
 
-TEST(IOTest, LoadsEdgeWeightedK3Correctly) {
-    const auto G = metis::read(test::test_instance("edge_weighted_K3.graph"));
+TEST(IOTest, edge_weighted_K3) {
+    const auto G = io::metis::read(test::test_instance("edge_weighted_K3.graph"));
     assert_K3_structure(G);
 
     ASSERT_THAT(outgoing_edge_weights(G, 0), UnorderedElementsAre(1, 2));
@@ -53,8 +54,8 @@ TEST(IOTest, LoadsEdgeWeightedK3Correctly) {
     ASSERT_THAT(outgoing_edge_weights(G, 2), UnorderedElementsAre(2, 3));
 }
 
-TEST(IOTest, LoadsWeightedK3Correctly) {
-    const auto G = metis::read(test::test_instance("weighted_K3.graph"));
+TEST(IOTest, weighted_K3) {
+    const auto G = io::metis::read(test::test_instance("weighted_K3.graph"));
     assert_K3_structure(G);
 
     ASSERT_THAT(G.node_weight(0), Eq(10));
@@ -66,19 +67,18 @@ TEST(IOTest, LoadsWeightedK3Correctly) {
     ASSERT_THAT(outgoing_edge_weights(G, 2), UnorderedElementsAre(2, 3));
 }
 
-TEST(IOTest, ParsesLargeNumbersCorrectly) {
-    const auto G = metis::read(test::test_instance("large_weights.graph"));
+TEST(IOTest, large_weights) {
+    const auto G = io::metis::read(test::test_instance("large_weights.graph"));
 
     ASSERT_THAT(G.node_weight(0), Eq(123456789));
     ASSERT_THAT(G.node_weight(1), Eq(234567891));
 }
 
-TEST(IOTest, IgnoresComments) {
-    const auto G = metis::read(test::test_instance("with_comments.graph"));
+TEST(IOTest, graph_with_comments) {
+    const auto G = io::metis::read(test::test_instance("with_comments.graph"));
 
     ASSERT_THAT(G.n(), Eq(2));
     ASSERT_THAT(G.m(), Eq(2));
     ASSERT_THAT(test::view_to_vector(G.adjacent_nodes(0)), UnorderedElementsAre(1));
     ASSERT_THAT(test::view_to_vector(G.adjacent_nodes(1)), UnorderedElementsAre(0));
 }
-} // namespace kaminpar::io
