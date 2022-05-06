e85c845695d82bf9bd31a40a18f0a19d54633273
Daniel Seemaier <daniel.seemaier@kit.edu>
2022-05-06 11:16:13 +0200

    Update build script

--- build.sh
+++ build.sh
@@ -11,33 +11,28 @@ function get_num_cores {
     ;;
   esac
 }
-NCORES=$(get_num_cores)
-[ -n "$NCORES" ] || NCORES=4
+
+# Only use detected core count if CMAKE_BUILD_PARALLEL_LEVEL is not set 
+if [[ -z "$CMAKE_BUILD_PARALLEL_LEVEL" ]]; then 
+    NCORES=$(get_num_cores)
+else 
+    NCORES="$CMAKE_BUILD_PARALLEL_LEVEL"
+fi 
 
 function build_target {
-  cmake --build "$BUILD_DIR" --parallel "$NCORES" --target $1
+  cmake --build "$BUILD_DIR" --parallel "$NCORES" --target "$1"
 }
 
+# Update submodules 
 git submodule update --init --recursive
 
-PROJECT_ROOT=$(pwd)
-if [ ! -f build/Makefile ]; then
-  ADDITIONAL_COMMANDS=""
+# Run CMake
+cmake -B "$BUILD_DIR" -DCMAKE_BUILD_TYPE=Release $ADDITIONAL_COMMANDS
 
-  mkdir -p "$BUILD_DIR" &&
-    cd "$BUILD_DIR" &&
-    cmake .. -DCMAKE_BUILD_TYPE=Release $ADDITIONAL_COMMANDS
-  cd "$PROJECT_ROOT" || exit
-fi
-if [ ! -f build/Makefile ]; then
-  echo "Unable to create Makefile in build/"
-  exit
-fi
-
+# Build targets
 build_target "KaMinPar" # binary
 build_target "kaminpar" # library
-
 if cmake --build "$BUILD_DIR" --target help | grep dKaMinPar; then
-    build_target "dKaMinPar"
+    build_target "dKaMinPar" # distributed binary
 fi
 
