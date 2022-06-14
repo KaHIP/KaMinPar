7eab30b78d8b0e3341856ceb9e5ad75b0807ce2e
Daniel Seemaier <daniel.seemaier@kit.edu>
2022-06-14 16:01:08 +0200

    Add square en/decoding

---
+++ common/utils/math.h
@@ -0,0 +1,26 @@
+/*******************************************************************************
+ * @file:   math.h
+ *
+ * @author: Daniel Seemaier
+ * @date:   14.06.2022
+ * @brief:  Utility functions for common (but simple) computations.
+ ******************************************************************************/
+#pragma once
+
+#include <cmath>
+#include <utility>
+#include <math.h>
+
+namespace kaminpar {
+template <typename Int>
+Int encode_virtual_square_position(const Int row, const Int column, const Int num_elements) {
+    const Int floor_sqrt = static_cast<Int>(std::floor(std::sqrt(num_elements)));
+    return row * floor_sqrt + column;
+}
+
+template <typename Int>
+std::pair<Int, Int> decode_virtual_square_position(const Int position, const Int num_elements) {
+    const Int floor_sqrt = static_cast<Int>(std::floor(std::sqrt(num_elements)));
+    return {position / floor_sqrt, position % floor_sqrt};
+}
+} // namespace kaminpar
---
+++ tests/common/utils/math_test.cc
@@ -0,0 +1,89 @@
+#include <gtest/gtest.h>
+
+#include "common/utils/math.h"
+
+using namespace kaminpar;
+
+TEST(MathTest, encode_virtual_square_2x2elements) {
+    const int num_elements = 4;
+    EXPECT_EQ(encode_virtual_square_position(0, 0, num_elements), 0);
+    EXPECT_EQ(encode_virtual_square_position(0, 1, num_elements), 1);
+    EXPECT_EQ(encode_virtual_square_position(1, 0, num_elements), 2);
+    EXPECT_EQ(encode_virtual_square_position(1, 1, num_elements), 3);
+}
+
+TEST(MathTest, decode_virtual_square_2x2elements) {
+    const int num_elements = 4;
+    EXPECT_EQ(decode_virtual_square_position(0, num_elements), std::make_pair(0, 0));
+    EXPECT_EQ(decode_virtual_square_position(1, num_elements), std::make_pair(0, 1));
+    EXPECT_EQ(decode_virtual_square_position(2, num_elements), std::make_pair(1, 0));
+    EXPECT_EQ(decode_virtual_square_position(3, num_elements), std::make_pair(1, 1));
+}
+
+TEST(MathTest, encode_virtual_square_3x3elements) {
+    const int num_elements = 9;
+    EXPECT_EQ(encode_virtual_square_position(0, 0, num_elements), 0);
+    EXPECT_EQ(encode_virtual_square_position(0, 1, num_elements), 1);
+    EXPECT_EQ(encode_virtual_square_position(0, 2, num_elements), 2);
+    EXPECT_EQ(encode_virtual_square_position(1, 0, num_elements), 3);
+    EXPECT_EQ(encode_virtual_square_position(1, 1, num_elements), 4);
+    EXPECT_EQ(encode_virtual_square_position(1, 2, num_elements), 5);
+    EXPECT_EQ(encode_virtual_square_position(2, 0, num_elements), 6);
+    EXPECT_EQ(encode_virtual_square_position(2, 1, num_elements), 7);
+    EXPECT_EQ(encode_virtual_square_position(2, 2, num_elements), 8);
+}
+
+TEST(MathTest, decode_virtual_square_3x3elements) {
+    const int num_elements = 9;
+    EXPECT_EQ(decode_virtual_square_position(0, num_elements), std::make_pair(0, 0));
+    EXPECT_EQ(decode_virtual_square_position(1, num_elements), std::make_pair(0, 1));
+    EXPECT_EQ(decode_virtual_square_position(2, num_elements), std::make_pair(0, 2));
+    EXPECT_EQ(decode_virtual_square_position(3, num_elements), std::make_pair(1, 0));
+    EXPECT_EQ(decode_virtual_square_position(4, num_elements), std::make_pair(1, 1));
+    EXPECT_EQ(decode_virtual_square_position(5, num_elements), std::make_pair(1, 2));
+    EXPECT_EQ(decode_virtual_square_position(6, num_elements), std::make_pair(2, 0));
+    EXPECT_EQ(decode_virtual_square_position(7, num_elements), std::make_pair(2, 1));
+    EXPECT_EQ(decode_virtual_square_position(8, num_elements), std::make_pair(2, 2));
+}
+
+TEST(MathTest, encode_virtual_square_5elements) {
+    const int num_elements = 5;
+    EXPECT_EQ(encode_virtual_square_position(0, 0, num_elements), 0);
+    EXPECT_EQ(encode_virtual_square_position(0, 1, num_elements), 1);
+    EXPECT_EQ(encode_virtual_square_position(1, 0, num_elements), 2);
+    EXPECT_EQ(encode_virtual_square_position(1, 1, num_elements), 3);
+    EXPECT_EQ(encode_virtual_square_position(2, 0, num_elements), 4);
+}
+
+TEST(MathTest, decode_virtual_square_5elements) {
+    const int num_elements = 5;
+    EXPECT_EQ(decode_virtual_square_position(0, num_elements), std::make_pair(0, 0));
+    EXPECT_EQ(decode_virtual_square_position(1, num_elements), std::make_pair(0, 1));
+    EXPECT_EQ(decode_virtual_square_position(2, num_elements), std::make_pair(1, 0));
+    EXPECT_EQ(decode_virtual_square_position(3, num_elements), std::make_pair(1, 1));
+    EXPECT_EQ(decode_virtual_square_position(4, num_elements), std::make_pair(2, 0));
+}
+
+TEST(MathTest, encode_virtual_square_8elements) {
+    const int num_elements = 8;
+    EXPECT_EQ(encode_virtual_square_position(0, 0, num_elements), 0);
+    EXPECT_EQ(encode_virtual_square_position(0, 1, num_elements), 1);
+    EXPECT_EQ(encode_virtual_square_position(1, 0, num_elements), 2);
+    EXPECT_EQ(encode_virtual_square_position(1, 1, num_elements), 3);
+    EXPECT_EQ(encode_virtual_square_position(2, 0, num_elements), 4);
+    EXPECT_EQ(encode_virtual_square_position(2, 1, num_elements), 5);
+    EXPECT_EQ(encode_virtual_square_position(3, 0, num_elements), 6);
+    EXPECT_EQ(encode_virtual_square_position(3, 1, num_elements), 7);
+}
+
+TEST(MathTest, decode_virtual_square_8elements) {
+    const int num_elements = 8;
+    EXPECT_EQ(decode_virtual_square_position(0, num_elements), std::make_pair(0, 0));
+    EXPECT_EQ(decode_virtual_square_position(1, num_elements), std::make_pair(0, 1));
+    EXPECT_EQ(decode_virtual_square_position(2, num_elements), std::make_pair(1, 0));
+    EXPECT_EQ(decode_virtual_square_position(3, num_elements), std::make_pair(1, 1));
+    EXPECT_EQ(decode_virtual_square_position(4, num_elements), std::make_pair(2, 0));
+    EXPECT_EQ(decode_virtual_square_position(5, num_elements), std::make_pair(2, 1));
+    EXPECT_EQ(decode_virtual_square_position(6, num_elements), std::make_pair(3, 0));
+    EXPECT_EQ(decode_virtual_square_position(7, num_elements), std::make_pair(3, 1));
+}
