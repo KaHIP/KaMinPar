aad4e0633cd97d67575c69377ef2ca4045cd5472
Daniel Seemaier <daniel+github@seemaier.de>
2022-07-26 15:37:18 +0200

    Change license to MIT

--- kaminpar/refinement/parallel_label_propagation_refiner.h
+++ kaminpar/refinement/parallel_label_propagation_refiner.h
@@ -1,22 +1,3 @@
-/*******************************************************************************
- * This file is part of KaMinPar.
- *
- * Copyright (C) 2021 Daniel Seemaier <daniel.seemaier@kit.edu>
- *
- * KaMinPar is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * KaMinPar is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with KaMinPar.  If not, see <http://www.gnu.org/licenses/>.
- *
-******************************************************************************/
 #pragma once
 
 #include "algorithm/parallel_label_propagation.h"
