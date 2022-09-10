4f604c0c58b487140fb984b7e29b57d12fe9a3a3
Daniel Seemaier <daniel.seemaier@kit.edu>
2022-09-11 00:28:57 +0200

    Fix errors

--- dkaminpar/mpi/grid_alltoall.h
+++ dkaminpar/mpi/grid_alltoall.h
@@ -34,7 +34,9 @@ public:
         GridTopology topo(size);
         DBG << rank << " --> row=" << topo.row(rank) << " col=" << topo.virtual_col(rank);
         MPI_Comm_split(comm, topo.row(rank), rank, &_row_comm);
-        MPI_Comm_split(comm, topo.virtual_col(rank), topo.virtual_col(rank) == topo.col(rank) ? rank : size + rank, &_col_comm);
+        MPI_Comm_split(
+            comm, topo.virtual_col(rank), topo.virtual_col(rank) == topo.col(rank) ? rank : size + rank, &_col_comm
+        );
     }
 
     MPI_Comm row_comm() const {
@@ -86,29 +88,31 @@ void sparse_alltoall_grid(SendBuffer&& data, const CountsBuffer& counts, Receive
 
     DBG << V(my_row) << V(my_col) << V(my_virtual_col) << V(row_comm_size) << V(col_comm_size);
 
+    /*
+     * Step 1 (column)
+     *
+     * For each row, send counts for all PEs in that row to the PE in our column in that row
+     */
+
     static std::vector<int> row_counts_send_counts;
     static std::vector<int> row_counts_recv_counts;
     static std::vector<int> row_counts_send_displs;
     static std::vector<int> row_counts_recv_displs;
     if (row_counts_send_counts.empty()) {
         // Compute send counts
-        DBG << "a";
         row_counts_send_counts.resize(col_comm_size);
         for (PEID row = 0; row < topo.num_rows(); ++row) {
             row_counts_send_counts[row] = topo.num_cols_in_row(row);
         }
 
-        DBG << "b";
-
         // Compute recv counts
         row_counts_recv_counts.resize(col_comm_size);
         if (my_col == my_virtual_col) {
             std::fill(row_counts_recv_counts.begin(), row_counts_recv_counts.end(), topo.row_size(my_row));
-        } else {
-            std::fill(row_counts_recv_counts.begin(), row_counts_recv_counts.end(), 0);
         }
-
-        DBG << "c";
+        // else {
+        //     std::fill(row_counts_recv_counts.begin(), row_counts_recv_counts.end(), 0);
+        // }
 
         // Compute buffer displacements
         row_counts_send_displs.resize(col_comm_size + 1);
@@ -121,45 +125,52 @@ void sparse_alltoall_grid(SendBuffer&& data, const CountsBuffer& counts, Receive
         );
     }
 
-    DBG << "alive 0";
-
     // Exchange counts within payload
-    std::vector<int> row_counts(topo.row_size(my_row) * col_comm_size);
-
-    DBG << V(rank) << V(row_counts_send_counts) << V(row_counts_send_displs) << V(row_counts_recv_counts) << V(row_counts_recv_displs);
-
+    std::vector<int> row_counts(row_comm_size * col_comm_size);
     KASSERT(row_counts.size() >= row_counts_recv_displs.back());
     KASSERT(row_counts_send_counts.size() >= col_comm_size);
     KASSERT(counts.size() >= row_counts_send_displs.back());
     KASSERT(row_counts_recv_counts.size() >= col_comm_size);
     KASSERT(row_counts_recv_displs.size() >= col_comm_size);
     KASSERT(row_counts_send_displs.size() >= col_comm_size);
-
     mpi::alltoallv(
         counts.data(), row_counts_send_counts.data(), row_counts_send_displs.data(), row_counts.data(),
         row_counts_recv_counts.data(), row_counts_recv_displs.data(), col_comm
     );
 
-    DBG << "alive 1 " << rank;
+    /*
+     * After Step 1 (column)
+     *
+     * row_counts contains data counts for PEs in my row:
+     * from PE in row 0: data count for col0, col1, col2, ...,  (row_comm_size items)
+     * from PE in row 1: data count for col0, col1, col2, ...,  (row_comm_size items)
+     * ...
+     *
+     * Size: row_comm_size * col_comm_size
+     */
 
-    // 1st hop send/recv counts/displs
+    /*
+     * Step 2 (column)
+     *
+     * Exchange data within our column: send PE in row 0 all data for row 0, ...
+     */
+
     std::vector<int> row_send_counts(col_comm_size);
     for (PEID pe = 0; pe < size; ++pe) {
         const PEID row = topo.row(pe);
+
+        KASSERT(row < row_send_counts.size());
         row_send_counts[row] += counts[pe];
     }
 
-    DBG << "alive 2";
-
     std::vector<int> row_recv_counts(col_comm_size);
     for (PEID row = 0; row < col_comm_size; ++row) {
-        for (PEID col = 0; col < topo.row_size(my_row); ++col) {
-            const PEID pe = row * col_comm_size + col;
-            row_recv_counts[row] += row_counts[pe];
-        }
+        KASSERT(row * row_comm_size < row_counts.size());
+        KASSERT((row + 1) * row_comm_size <= row_counts.size());
+        row_recv_counts[row] = std::accumulate(
+            row_counts.begin() + row * row_comm_size, row_counts.begin() + (row + 1) * row_comm_size, 0
+        );
     }
-
-    DBG << "alive 3";
 
     std::vector<int> row_send_displs(grid_size + 1);
     std::vector<int> row_recv_displs(grid_size + 1);
@@ -167,15 +178,15 @@ void sparse_alltoall_grid(SendBuffer&& data, const CountsBuffer& counts, Receive
     std::partial_sum(row_recv_counts.begin(), row_recv_counts.end(), row_recv_displs.begin() + 1);
 
     // Exchange 1st hop payload
-    std::vector<Message> row_recv_buf(row_recv_displs.back());
+    DBG << V(row_recv_displs);
 
+    std::vector<Message> row_recv_buf(row_recv_displs.back());
     KASSERT(row_send_counts.size() >= col_comm_size);
     KASSERT(row_send_displs.size() >= col_comm_size);
     KASSERT(data.size() >= row_send_displs.back());
     KASSERT(row_recv_counts.size() >= col_comm_size);
     KASSERT(row_recv_displs.size() >= col_comm_size);
     KASSERT(row_recv_buf.size() >= row_recv_displs.back());
-
     mpi::alltoallv(
         data.data(), row_send_counts.data(), row_send_displs.data(), row_recv_buf.data(), row_recv_counts.data(),
         row_recv_displs.data(), col_comm
@@ -186,14 +197,27 @@ void sparse_alltoall_grid(SendBuffer&& data, const CountsBuffer& counts, Receive
 
     DBG << V(row_recv_buf) << V(row_counts);
 
-    // Assertion:
-    // row_recv_buf containts data for each PE in the same row as this PE:
-    // col1, ..., coln, col1, ..., coln, ... -- size: col size * row size
-    // The sizes are given by row_counts:
-    // size1, ..., sizen, size1, ..., sizen, ... -- size: col size * row size
-    // The displacements are given by row_displs, thus, the data for PE 1 (in
-    // row_comm) is given by row_data[displs(1) ... displs(1) + size(1)] AND
-    // row_data[displs(n+1) ... displs(n+1) + size(n+1)] AND ...
+    /*
+     * After step 2 (column)
+     *
+     * row_recv_buf contains data for PEs in my row:
+     * data from PEs in row 0 for: col0, col1, col2, ...,  (row_comm_size items)
+     * data from PEs in row 1 for: col0, col1, col2, ...,  (row_comm_size items)
+     * ...                                                 (column_comm_size rows)
+     *
+     * The data count is determined by row_coutns:
+     * data counts from PEs in row0: col0, col1, col2, ...,
+     * data counts from PEs in row1: col0, col1, col2, ...,
+     * ...
+     *
+     * Size: row_comm_size * col_comm_size
+     */
+
+    /*
+     * Step 3 (row)
+     *
+     * Send each column the data counts from each row
+     */
 
     // total data count for each PE in my row
     std::vector<int> col_counts(row_comm_size);
@@ -203,6 +227,10 @@ void sparse_alltoall_grid(SendBuffer&& data, const CountsBuffer& counts, Receive
     for (PEID col = 0; col < row_comm_size; ++col) {
         for (PEID from_row = 0; from_row < col_comm_size; ++from_row) {
             const PEID pe = col + from_row * row_comm_size;
+            KASSERT(col < col_counts.size());
+            KASSERT(pe < row_counts.size());
+            KASSERT(from_row + col * col_comm_size < col_subcounts.size());
+
             col_counts[col] += row_counts[pe];
             col_subcounts[from_row + col * col_comm_size] = row_counts[pe];
         }
@@ -214,10 +242,13 @@ void sparse_alltoall_grid(SendBuffer&& data, const CountsBuffer& counts, Receive
     static std::vector<int> col_subcounts_recv_displs;
     if (col_subcounts_send_counts.empty()) {
         col_subcounts_send_counts.resize(row_comm_size);
-        std::fill(col_subcounts_send_counts.begin(), col_subcounts_send_counts.end(), col_comm_size);
+        if (topo.col(rank) == topo.virtual_col(rank)) {
+            std::fill(col_subcounts_send_counts.begin(), col_subcounts_send_counts.end(), col_comm_size);
+        }
 
         col_subcounts_recv_counts.resize(row_comm_size);
         for (PEID col = 0; col < topo.num_full_cols(); ++col) {
+            KASSERT(col < col_subcounts_recv_counts.size());
             col_subcounts_recv_counts[col] = topo.virtual_col_size(col);
         }
 
@@ -252,18 +283,26 @@ void sparse_alltoall_grid(SendBuffer&& data, const CountsBuffer& counts, Receive
         col_subcounts_recv_counts.data(), col_subcounts_recv_displs.data(), row_comm
     );
 
+    DBG << "OK SO FAR?";
+
     DBG << V(subcounts);
+
+    DBG << "OK SO FAR";
 
     // Assertion:
     // subcounts
 
     std::vector<int> col_recv_counts(row_comm_size);
-    PEID _p = 0;
+    PEID             _p = 0;
     for (PEID row = 0; row < topo.num_rows(); ++row) {
         int sum = 0;
+
         for (PEID col = 0; col < topo.num_cols_in_row(row); ++col) {
+            KASSERT(_p < subcounts.size());
             sum += subcounts[_p++];
         }
+
+        KASSERT(row < col_recv_counts.size());
         col_recv_counts[row] = sum;
     }
 
@@ -275,11 +314,22 @@ void sparse_alltoall_grid(SendBuffer&& data, const CountsBuffer& counts, Receive
 
     std::vector<Message> col_data(row_recv_buf.size());
     for (PEID col = 0; col < row_comm_size; ++col) {
+        KASSERT(col < col_displs.size());
         int i = col_displs[col];
+
         for (PEID row = 0; row < col_comm_size; ++row) {
             const PEID pe        = row * row_comm_size + col;
+
+            KASSERT(pe < row_displs.size());
+            KASSERT(pe < row_counts.size());
+
             const int  row_displ = row_displs[pe];
             const int  row_count = row_counts[pe];
+
+            KASSERT(row_displ <= row_recv_buf.size());
+            KASSERT(row_displ + row_count <= row_recv_buf.size());
+            KASSERT(i <= col_data.size());
+            KASSERT(i + row_count <= col_data.size());
 
             std::copy(
                 row_recv_buf.begin() + row_displ, row_recv_buf.begin() + row_displ + row_count, col_data.begin() + i
