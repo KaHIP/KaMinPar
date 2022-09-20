62432cee03157947eed7e133679eab953d6f56ec
Daniel Seemaier <daniel.seemaier@kit.edu>
2022-09-20 14:10:40 +0200

    Fix crash during IP

--- dkaminpar/initial_partitioning/kaminpar_initial_partitioner.cc
+++ dkaminpar/initial_partitioning/kaminpar_initial_partitioner.cc
@@ -6,14 +6,23 @@
  ******************************************************************************/
 #include "dkaminpar/initial_partitioning/kaminpar_initial_partitioner.h"
 
+#include "kaminpar/datastructure/graph.h"
 #include "kaminpar/partitioning_scheme/partitioning.h"
 
+#include "common/datastructures/static_array.h"
 #include "common/logger.h"
+#include "common/scalable_vector.h"
 #include "common/timer.h"
 
 namespace kaminpar::dist {
 shm::PartitionedGraph
 KaMinParInitialPartitioner::initial_partition(const shm::Graph& graph, const PartitionContext& p_ctx) {
+    if (graph.n() <= 1) {
+        return shm::PartitionedGraph(
+            graph, p_ctx.k, StaticArray<BlockID>(graph.n()), scalable_vector<BlockID>(p_ctx.k, 1)
+        );
+    }
+
     auto shm_ctx                         = _ctx.initial_partitioning.kaminpar;
     shm_ctx.refinement.lp.num_iterations = 1;
     shm_ctx.partition.k                  = p_ctx.k;
