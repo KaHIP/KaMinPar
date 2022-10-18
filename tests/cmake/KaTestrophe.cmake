d3d9b4658241bb3a683db9126f4a8981a030c3bd
Daniel Seemaier <daniel.seemaier@kit.edu>
2022-10-18 14:15:20 +0200

    Rename extern to external

--- tests/cmake/KaTestrophe.cmake
+++ tests/cmake/KaTestrophe.cmake
@@ -4,7 +4,7 @@ if (NOT DEFINED KATESTROPHE_INCLUDED)
     include(MPIGoogleTest)
 
     # gtest-mpi-listener does not use modern CMake, therefore we need this fix
-    set(gtest-mpi-listener_SOURCE_DIR ${CMAKE_SOURCE_DIR}/extern/gtest-mpi-listener)
+    set(gtest-mpi-listener_SOURCE_DIR ${CMAKE_SOURCE_DIR}/external/gtest-mpi-listener)
     add_library(gtest-mpi-listener INTERFACE)
     target_include_directories(gtest-mpi-listener INTERFACE "${gtest-mpi-listener_SOURCE_DIR}")
     target_link_libraries(gtest-mpi-listener INTERFACE MPI::MPI_CXX gtest gmock)
