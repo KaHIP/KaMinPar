a9102381ea3f1c321e492bc5ec9c9c886cf00716
Daniel Seemaier <daniel@seemaier.de>
2023-04-18 14:46:52 +0200

    Reformat

--- cli/kaminpar_arguments.h
+++ cli/kaminpar_arguments.h
@@ -3,7 +3,7 @@
  * @author: Daniel Seemaier
  * @date:   14.10.2022
  ******************************************************************************/
-#pragma once 
+#pragma once
 
 // clang-format off
 #include "CLI11.h"
@@ -13,16 +13,25 @@
 
 namespace kaminpar::shm {
 void create_all_options(CLI::App *app, Context &ctx);
+
 CLI::Option_group *create_partitioning_options(CLI::App *app, Context &ctx);
+
 CLI::Option_group *create_coarsening_options(CLI::App *app, Context &ctx);
+
 CLI::Option_group *create_lp_coarsening_options(CLI::App *app, Context &ctx);
+
 CLI::Option_group *
 create_initial_partitioning_options(CLI::App *app, Context &ctx);
+
 CLI::Option_group *
 create_initial_refinement_options(CLI::App *app, Context &ctx);
+
 CLI::Option_group *
 create_initial_fm_refinement_options(CLI::App *app, Context &ctx);
+
 CLI::Option_group *create_refinement_options(CLI::App *app, Context &ctx);
+
 CLI::Option_group *create_lp_refinement_options(CLI::App *app, Context &ctx);
+
 CLI::Option_group *create_debug_options(CLI::App *app, Context &ctx);
 } // namespace kaminpar::shm
