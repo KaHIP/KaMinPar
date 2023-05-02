8605eb7a4b4353dce903e9e58f9a8858b734c85f
Daniel Seemaier <daniel@seemaier.de>
2023-05-02 14:04:55 +0200

    Attempt to improve JET running time

--- kaminpar/refinement/jet_refiner.cc
+++ kaminpar/refinement/jet_refiner.cc
@@ -14,84 +14,6 @@ namespace kaminpar::shm {
 SET_DEBUG(true);
 SET_STATISTICS(true);
 
-namespace {
-void perform_iteration(
-    PartitionedGraph &p_graph,
-    const PartitionContext &p_ctx,
-    const double c,
-    NoinitVector<std::uint8_t> &lock
-) {
-  DenseGainCache gain_cache(p_graph.k(), p_graph.n());
-  gain_cache.initialize(p_graph);
-
-  NoinitVector<BlockID> next_partition(p_graph.n());
-  p_graph.pfor_nodes([&](const NodeID u) {
-    const BlockID from = p_graph.block(u);
-
-    EdgeWeight best_gain = std::numeric_limits<EdgeWeight>::min();
-    BlockID best_block = from;
-
-    for (const BlockID to : p_graph.blocks()) {
-      if (to == from) {
-        continue;
-      }
-
-      const EdgeWeight gain = gain_cache.gain(u, from, to);
-      if (gain > best_gain) {
-        best_gain = gain;
-        best_block = to;
-      }
-    }
-
-    KASSERT(u < lock.size());
-    if (!lock[u] && -best_gain < std::floor(c * gain_cache.conn(u, from))) {
-      next_partition[u] = best_block;
-    } else {
-      next_partition[u] = from;
-    }
-  });
-
-  p_graph.pfor_nodes([&](const NodeID u) { lock[u] = 0; });
-
-  p_graph.pfor_nodes([&](const NodeID u) {
-    const BlockID from = p_graph.block(u);
-    const BlockID to = next_partition[u];
-    if (from == to) {
-      return;
-    }
-
-    const EdgeWeight gain_u = gain_cache.gain(u, from, to);
-    EdgeWeight gain = 0;
-
-    for (const auto &[e, v] : p_graph.neighbors(u)) {
-      const EdgeWeight weight = p_graph.edge_weight(e);
-
-      const bool v_before_u = [&, v = v] {
-        const BlockID from_v = p_graph.block(v);
-        const BlockID to_v = next_partition[v];
-        if (from_v != to_v) {
-          const EdgeWeight gain_v = gain_cache.gain(v, from_v, to_v);
-          return gain_v > gain_u || (gain_v == gain_u && v < u);
-        }
-        return false;
-      }();
-      const BlockID block_v = v_before_u ? next_partition[v] : p_graph.block(v);
-
-      if (to == block_v) {
-        gain += weight;
-      } else if (from == block_v) {
-        gain -= weight;
-      }
-    }
-
-    if (gain > 0) {
-      p_graph.set_block(u, to);
-      lock[u] = 1;
-    }
-  });
-}
-} // namespace
-
 JetRefiner::JetRefiner(const Context &ctx) : _ctx(ctx) {}
 
 bool JetRefiner::refine(
@@ -118,19 +40,99 @@ bool JetRefiner::refine(
   }();
   DBG << "Set c=" << c;
 
+  TIMED_SCOPE("Statistics") {
+    const EdgeWeight initial_cut = IFDBG(metrics::edge_cut(p_graph));
+    const double initial_balance = IFDBG(metrics::imbalance(p_graph));
+    const bool initial_feasible = IFDBG(metrics::is_feasible(p_graph, p_ctx));
+    DBG << "Initial cut=" << initial_cut << ", imbalance=" << initial_balance
+        << ", feasible=" << initial_feasible;
+  };
+
+  START_TIMER("Allocation");
+  GreedyBalancer balancer(_ctx);
+  balancer.initialize(p_graph);
+
+  DenseGainCache gain_cache(p_graph.k(), p_graph.n());
+  gain_cache.initialize(p_graph);
+
+  NoinitVector<BlockID> next_partition(p_graph.n());
+  p_graph.pfor_nodes([&](const NodeID u) { next_partition[u] = 0; });
+
   NoinitVector<std::uint8_t> lock(p_graph.n());
   p_graph.pfor_nodes([&](const NodeID u) { lock[u] = 0; });
-
-  const EdgeWeight initial_cut = IFDBG(metrics::edge_cut(p_graph));
-  const double initial_balance = IFDBG(metrics::imbalance(p_graph));
-  const bool initial_feasible = IFDBG(metrics::is_feasible(p_graph, p_ctx));
-  DBG << "Initial cut=" << initial_cut << ", imbalance=" << initial_balance
-      << ", feasible=" << initial_feasible;
-
-  GreedyBalancer balancer(_ctx);
+  STOP_TIMER();
 
   for (int i = 0; i < _ctx.refinement.jet.num_iterations; ++i) {
-    perform_iteration(p_graph, p_ctx, c, lock);
+    p_graph.pfor_nodes([&](const NodeID u) {
+      const BlockID from = p_graph.block(u);
+
+      if (lock[u] || !gain_cache.is_border_node(u, from)) {
+        next_partition[u] = from;
+        return;
+      }
+
+      EdgeWeight best_gain = std::numeric_limits<EdgeWeight>::min();
+      BlockID best_block = from;
+
+      for (const BlockID to : p_graph.blocks()) {
+        if (to == from) {
+          continue;
+        }
+
+        const EdgeWeight gain = gain_cache.gain(u, from, to);
+        if (gain > best_gain) {
+          best_gain = gain;
+          best_block = to;
+        }
+      }
+
+      if (-best_gain < std::floor(c * gain_cache.conn(u, from))) {
+        next_partition[u] = best_block;
+      } else {
+        next_partition[u] = from;
+      }
+    });
+
+    p_graph.pfor_nodes([&](const NodeID u) {
+      const BlockID from = p_graph.block(u);
+      const BlockID to = next_partition[u];
+      if (from == to) {
+        return;
+      }
+
+      const EdgeWeight gain_u = gain_cache.gain(u, from, to);
+      EdgeWeight gain = 0;
+
+      for (const auto &[e, v] : p_graph.neighbors(u)) {
+        const EdgeWeight weight = p_graph.edge_weight(e);
+
+        const bool v_before_u = [&, v = v] {
+          const BlockID from_v = p_graph.block(v);
+          const BlockID to_v = next_partition[v];
+          if (from_v != to_v) {
+            const EdgeWeight gain_v = gain_cache.gain(v, from_v, to_v);
+            return gain_v > gain_u || (gain_v == gain_u && v < u);
+          }
+          return false;
+        }();
+        const BlockID block_v =
+            v_before_u ? next_partition[v] : p_graph.block(v);
+
+        if (to == block_v) {
+          gain += weight;
+        } else if (from == block_v) {
+          gain -= weight;
+        }
+      }
+
+      if (gain > 0) {
+        p_graph.set_block(u, to);
+        gain_cache.move(p_graph, u, from, to);
+        lock[u] = 1;
+      } else {
+        lock[u] = 0;
+      }
+    });
 
     const EdgeWeight pre_rebalance_cut = IFDBG(metrics::edge_cut(p_graph));
     const double pre_rebalance_balance = IFDBG(metrics::imbalance(p_graph));
@@ -142,7 +144,6 @@ bool JetRefiner::refine(
         << ", imbalance=" << pre_rebalance_balance
         << ", feasible=" << pre_rebalance_feasible;
 
-    balancer.initialize(p_graph);
     balancer.refine(p_graph, p_ctx);
 
     const EdgeWeight post_rebalance_cut = IFDBG(metrics::edge_cut(p_graph));
