1873eff1ffb7811d08b4a74dfab6fe7d3836e339
Daniel Seemaier <daniel@seemaier.de>
2023-06-01 16:33:58 +0200

    [Dist] Fix target cluster weights for node rollback during clustering

--- dkaminpar/coarsening/clustering/global_lp_clustering.cc
+++ dkaminpar/coarsening/clustering/global_lp_clustering.cc
@@ -474,8 +474,9 @@ private:
           const GlobalNodeWeight increase_by_me = (*it).second;
 
           violation = 1;
-          new_weight = _max_cluster_weight + (1.0 * increase_by_me / increase_by_others) *
-                                                 (new_weight - _max_cluster_weight);
+          new_weight =
+              _max_cluster_weight + (1.0 * increase_by_me / (increase_by_others + increase_by_me)) *
+                                        (new_weight - _max_cluster_weight);
         }
         change_cluster_weight(cluster, -old_weight + new_weight, true);
       });
