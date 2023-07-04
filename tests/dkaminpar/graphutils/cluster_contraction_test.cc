047b325fd35dc3947fb266cf156fc06c2ef373c0
Daniel Seemaier <daniel@seemaier.de>
2023-07-04 17:14:12 +0200

    Tests + Bugfix

--- tests/dkaminpar/graphutils/cluster_contraction_test.cc
+++ tests/dkaminpar/graphutils/cluster_contraction_test.cc
@@ -16,6 +16,40 @@ namespace kaminpar::dist {
 using namespace kaminpar::dist::testing;
 using namespace ::testing;
 
+namespace {
+StaticArray<GlobalNodeID> build_cnode_distribution(const GlobalNodeID n) {
+  return mpi::build_distribution_from_local_count<GlobalNodeID, StaticArray>(n, MPI_COMM_WORLD);
+}
+} // namespace
+
+TEST(ClusterReassignmentTest, perfectly_balanced_case) {
+  const auto graph = make_isolated_nodes_graph(4);
+  const auto cnode_distribution = build_cnode_distribution(2);
+  const auto result = compute_assignment_shifts(graph, cnode_distribution, 1.0);
+  EXPECT_THAT(result.overload, Each(Eq(0)));
+  EXPECT_THAT(result.underload, Each(Eq(0)));
+}
+
+TEST(ClusterReassignmentTest, stair_no_limit) {
+  const PEID rank = mpi::get_comm_rank(MPI_COMM_WORLD);
+  const PEID size = mpi::get_comm_size(MPI_COMM_WORLD);
+
+  const auto graph = make_isolated_nodes_graph(size * size);
+  const auto cnode_distribution = build_cnode_distribution(2 * (rank + 1));
+  const auto result = compute_assignment_shifts(graph, cnode_distribution, 1.0);
+
+  const GlobalNodeID expected = size + 1;
+
+  for (PEID pe = 0; pe < size / 2; ++pe) {
+    EXPECT_EQ(result.overload[pe + 1] - result.overload[pe], 0);
+    EXPECT_EQ(result.underload[pe + 1] - result.underload[pe], expected - 2 * (pe + 1));
+  }
+  for (PEID pe = size / 2; pe < size; ++pe) {
+    EXPECT_EQ(result.overload[pe + 1] - result.overload[pe], 2 * (pe + 1) - expected);
+    EXPECT_EQ(result.underload[pe + 1] - result.underload[pe], 0);
+  }
+}
+
 TEST(ClusterContractionTest, contract_empty_graph) {
   auto graph = make_empty_graph();
 
