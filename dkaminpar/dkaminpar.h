e5a9a053a171c5cf3c70fa2b248844e93422e105
Daniel Seemaier <daniel@seemaier.de>
2023-07-27 15:04:58 +0200

    Remove pointless default values in dkaminpar.h

--- dkaminpar/dkaminpar.h
+++ dkaminpar/dkaminpar.h
@@ -103,116 +103,115 @@ enum class GraphOrdering {
 };
 
 struct ParallelContext {
-  std::size_t num_threads = 0;
-  std::size_t num_mpis = 0;
+  std::size_t num_threads;
+  std::size_t num_mpis;
 };
 
 struct LabelPropagationCoarseningContext {
-  int num_iterations = 0;
-  NodeID passive_high_degree_threshold = 0;
-  NodeID active_high_degree_threshold = 0;
-  NodeID max_num_neighbors = 0;
-  bool merge_singleton_clusters = 0;
-  double merge_nonadjacent_clusters_threshold = 0;
-  int total_num_chunks = 0;
-  int fixed_num_chunks = 0;
-  int min_num_chunks = 0;
-  bool ignore_ghost_nodes = false;
-  bool keep_ghost_clusters = false;
-  bool scale_chunks_with_threads = false;
+  int num_iterations;
+  NodeID passive_high_degree_threshold;
+  NodeID active_high_degree_threshold;
+  NodeID max_num_neighbors;
+  bool merge_singleton_clusters;
+  double merge_nonadjacent_clusters_threshold;
+  int total_num_chunks;
+  int fixed_num_chunks;
+  int min_num_chunks;
+  bool ignore_ghost_nodes;
+  bool keep_ghost_clusters;
+  bool scale_chunks_with_threads;
 
-  bool sync_cluster_weights = false;
-  bool enforce_cluster_weights = false;
-  bool cheap_toplevel = false;
+  bool sync_cluster_weights;
+  bool enforce_cluster_weights;
+  bool cheap_toplevel;
 
-  bool prevent_cyclic_moves = false;
-  bool enforce_legacy_weight = false;
+  bool prevent_cyclic_moves;
+  bool enforce_legacy_weight;
 
   bool should_merge_nonadjacent_clusters(NodeID old_n, NodeID new_n) const;
   int compute_num_chunks(const ParallelContext &parallel) const;
 };
 
 struct HEMCoarseningContext {
-  int max_num_coloring_chunks = 0;
-  int fixed_num_coloring_chunks = 0;
-  int min_num_coloring_chunks = 0;
-  bool scale_coloring_chunks_with_threads = false;
-  double small_color_blacklist = 0;
-  bool only_blacklist_input_level = false;
-  bool ignore_weight_limit = false;
+  int max_num_coloring_chunks;
+  int fixed_num_coloring_chunks;
+  int min_num_coloring_chunks;
+  bool scale_coloring_chunks_with_threads;
+  double small_color_blacklist;
+  bool only_blacklist_input_level;
+  bool ignore_weight_limit;
 
   int compute_num_coloring_chunks(const ParallelContext &parallel) const;
 };
 struct ColoredLabelPropagationRefinementContext {
-  int num_iterations = 0;
-  int num_move_execution_iterations = 0;
-  int num_probabilistic_move_attempts = 0;
-  bool sort_by_rel_gain = false;
+  int num_iterations;
+  int num_move_execution_iterations;
+  int num_probabilistic_move_attempts;
+  bool sort_by_rel_gain;
 
-  int max_num_coloring_chunks = 0;
-  int fixed_num_coloring_chunks = 0;
-  int min_num_coloring_chunks = 0;
-  bool scale_coloring_chunks_with_threads = false;
-  double small_color_blacklist = 0;
-  bool only_blacklist_input_level = false;
+  int max_num_coloring_chunks;
+  int fixed_num_coloring_chunks;
+  int min_num_coloring_chunks;
+  bool scale_coloring_chunks_with_threads;
+  double small_color_blacklist;
+  bool only_blacklist_input_level;
 
-  bool track_local_block_weights = false;
-  bool use_active_set = false;
+  bool track_local_block_weights;
+  bool use_active_set;
 
-  LabelPropagationMoveExecutionStrategy move_execution_strategy =
-      LabelPropagationMoveExecutionStrategy::PROBABILISTIC;
+  LabelPropagationMoveExecutionStrategy move_execution_strategy;
 
   int compute_num_coloring_chunks(const ParallelContext &parallel) const;
 };
 
 struct LabelPropagationRefinementContext {
-  NodeID active_high_degree_threshold = 0;
-  int num_iterations = 0;
+  NodeID active_high_degree_threshold;
+  int num_iterations;
 
-  int total_num_chunks = 0;
-  int fixed_num_chunks = 0;
-  int min_num_chunks = 0;
+  int total_num_chunks;
+  int fixed_num_chunks;
+  int min_num_chunks;
 
-  int num_move_attempts = 0;
-  bool ignore_probabilities = false;
-  bool scale_chunks_with_threads = false;
+  int num_move_attempts;
+  bool ignore_probabilities;
+  bool scale_chunks_with_threads;
 
   int compute_num_chunks(const ParallelContext &parallel) const;
 };
 
 struct FMRefinementContext {
-  double alpha = 0.0;
-  NodeID radius = 0;
-  PEID pe_radius = 0;
-  bool overlap_regions = false;
-  std::size_t num_iterations = 0;
-  bool sequential = false;
-  bool premove_locally = false;
-  NodeID bound_degree = 0;
-  bool contract_border = false;
+  double alpha;
+  NodeID radius;
+  PEID pe_radius;
+  bool overlap_regions;
+  std::size_t num_iterations;
+  bool sequential;
+  bool premove_locally;
+  NodeID bound_degree;
+  bool contract_border;
 };
 
 struct CoarseningContext {
   // Global clustering
-  std::size_t max_global_clustering_levels = 0;
+  std::size_t max_global_clustering_levels;
   GlobalClusteringAlgorithm global_clustering_algorithm;
   LabelPropagationCoarseningContext global_lp;
   HEMCoarseningContext hem;
 
   // Local clustering
-  std::size_t max_local_clustering_levels = 0;
+  std::size_t max_local_clustering_levels;
   LocalClusteringAlgorithm local_clustering_algorithm;
   LabelPropagationCoarseningContext local_lp;
 
   // Cluster weight limit
-  NodeID contraction_limit = 0;
-  shm::ClusterWeightLimit cluster_weight_limit = shm::ClusterWeightLimit::EPSILON_BLOCK_WEIGHT;
-  double cluster_weight_multiplier = 0.0;
+  NodeID contraction_limit;
+  shm::ClusterWeightLimit cluster_weight_limit;
+  double cluster_weight_multiplier;
 
   // Graph contraction
-  double max_cnode_imbalance = std::numeric_limits<double>::max();
-  bool migrate_cnode_prefix = true;
-  bool force_perfect_cnode_balance = false;
+  double max_cnode_imbalance;
+  bool migrate_cnode_prefix;
+  bool force_perfect_cnode_balance;
 
   void setup(const ParallelContext &parallel);
 };
@@ -223,19 +222,19 @@ struct InitialPartitioningContext {
 };
 
 struct GreedyBalancerContext {
-  int max_num_rounds = 0;
-  bool enable_strong_balancing = false;
-  NodeID num_nodes_per_block = 0;
-  bool enable_fast_balancing = false;
-  double fast_balancing_threshold = 0.0;
+  int max_num_rounds;
+  bool enable_strong_balancing;
+  NodeID num_nodes_per_block;
+  bool enable_fast_balancing;
+  double fast_balancing_threshold;
 };
 
 enum class MoveSetSizeStrategy {
   ZERO,
   ONE,
   MAX_OVERLOAD,
-  MIN_OVERLOAD,
   AVG_OVERLOAD,
+  MIN_OVERLOAD,
 };
 
 enum class MoveSetStrategy {
@@ -244,40 +243,40 @@ enum class MoveSetStrategy {
 };
 
 struct MoveSetBalancerContext {
-  int max_num_rounds = 0;
+  int max_num_rounds;
 
-  bool enable_sequential_balancing = false;
-  NodeID seq_num_nodes_per_block = 0;
-  bool seq_full_pq = false;
+  bool enable_sequential_balancing;
+  NodeID seq_num_nodes_per_block;
+  bool seq_full_pq;
 
-  bool enable_parallel_balancing = false;
-  double parallel_threshold = 0.0;
+  bool enable_parallel_balancing;
+  double parallel_threshold;
 
-  MoveSetSizeStrategy move_set_size_strategy = MoveSetSizeStrategy::ZERO;
-  double move_set_size_multiplier = 0.0;
+  MoveSetSizeStrategy move_set_size_strategy;
+  double move_set_size_multiplier;
 
-  MoveSetStrategy move_set_strategy = MoveSetStrategy::SINGLETONS;
-  int move_set_rebuild_interval = 0;
+  MoveSetStrategy move_set_strategy;
+  int move_set_rebuild_interval;
 };
 
 struct JetBalancerContext {
-  int num_weak_iterations = 0;
-  int num_strong_iterations = 0;
+  int num_weak_iterations;
+  int num_strong_iterations;
 };
 
 struct JetRefinementContext {
-  int num_iterations = 0;
-  double min_c = 0.0;
-  double max_c = 0.0;
-  bool interpolate_c = false;
-  bool use_abortion_threshold = false;
-  double abortion_threshold = 0;
+  int num_iterations;
+  double min_c;
+  double max_c;
+  bool interpolate_c;
+  bool use_abortion_threshold;
+  double abortion_threshold;
   RefinementAlgorithm balancing_algorithm;
 };
 
 struct RefinementContext {
   std::vector<RefinementAlgorithm> algorithms;
-  bool refine_coarsest_level = false;
+  bool refine_coarsest_level;
 
   LabelPropagationRefinementContext lp;
   ColoredLabelPropagationRefinementContext colored_lp;
@@ -301,24 +300,24 @@ struct PartitionContext {
 
   BlockID k = kInvalidBlockID;
   BlockID K = kInvalidBlockID;
-  double epsilon = 0.0;
+  double epsilon;
 
   std::unique_ptr<struct GraphContext> graph;
 };
 
 struct DebugContext {
-  std::string graph_filename = "";
-  bool save_coarsest_graph = false;
-  bool save_coarsest_partition = false;
+  std::string graph_filename;
+  bool save_coarsest_graph;
+  bool save_coarsest_partition;
 };
 
 struct Context {
-  GraphOrdering rearrange_by = GraphOrdering::NATURAL;
+  GraphOrdering rearrange_by;
 
-  PartitioningMode mode = PartitioningMode::DEEP;
+  PartitioningMode mode;
 
-  bool enable_pe_splitting = false;
-  bool simulate_singlethread = false;
+  bool enable_pe_splitting;
+  bool simulate_singlethread;
 
   PartitionContext partition;
   ParallelContext parallel;
