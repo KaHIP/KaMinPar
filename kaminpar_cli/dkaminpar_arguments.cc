509f4685920cf5780cc3024ce114c8d4b8d05a09
Daniel Seemaier <daniel@seemaier.de>
2023-09-20 17:14:32 +0200

    [CLI] Rename --r-fm-use-bfs-seeds-as-fm-seeds to --r-fm-bfs-seeds-as-fm-seeds for more consistent naming

--- kaminpar_cli/dkaminpar_arguments.cc
+++ kaminpar_cli/dkaminpar_arguments.cc
@@ -199,7 +199,7 @@ CLI::Option_group *create_fm_refinement_options(CLI::App *app, Context &ctx) {
 
   fm->add_flag("--r-fm-independent-seeds", ctx.refinement.fm.use_independent_seeds)
       ->capture_default_str();
-  fm->add_flag("--r-fm-use-bfs-seeds-as-fm-seeds", ctx.refinement.fm.use_bfs_seeds_as_fm_seeds)
+  fm->add_flag("--r-fm-bfs-seeds-as-fm-seeds", ctx.refinement.fm.use_bfs_seeds_as_fm_seeds)
       ->capture_default_str();
 
   fm->add_option("--r-fm-max-hops", ctx.refinement.fm.max_hops)->capture_default_str();
