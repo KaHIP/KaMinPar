KaHIP/KaMinPar

commit fb6928076e1e92a85fdfd173872a567336e4fe94

[dKaMinPar:LP] Fix missing STOP_TIMER() when running without cluster weight control

kaminpar-dist/coarsening/clustering/lp/global_lp_clusterer.cc
@@ -355,7 +355,7 @@ private:
   }
 
   void control_cluster_weights(const NodeID from, const NodeID to) {
-    START_TIMER("Synchronize cluster weights");
+    SCOPED_TIMER("Synchronize cluster weights");
 
     if (!should_sync_cluster_weights()) {
       return;

commit b013e23dc8ad971be2fdd25af78a9c1aef034e63

[dKaMinPar:LP] Fix crash introduced in fb69280 due to extra STOP_TIMER()

kaminpar-dist/coarsening/clustering/lp/global_lp_clusterer.cc
@@ -491,8 +491,6 @@ private:
 
     mpi::barrier(_graph->communicator());
 
-    STOP_TIMER();
-
     // If we detected a max cluster weight violation, remove node weight
     // proportional to our chunk of the cluster weight
     if (!should_enforce_cluster_weights() || !violation) {