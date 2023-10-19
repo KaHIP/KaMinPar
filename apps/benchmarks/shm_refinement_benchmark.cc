43672edbc08f0069e68c73551e3ec6870f652af6
Daniel Seemaier <daniel@seemaier.de>
2023-10-19 14:13:00 +0200

    [Benchmarks] Avoid crash in shm_refinement_benchmark due to using timers after stopping the root timer

--- apps/benchmarks/shm_refinement_benchmark.cc
+++ apps/benchmarks/shm_refinement_benchmark.cc
@@ -51,27 +51,29 @@ int main(int argc, char *argv[]) {
   tbb::global_control gc(tbb::global_control::max_allowed_parallelism, num_threads);
 
   // Load input graph
-  auto input = load_partitioned_graph(graph_filename, partition_filename);
-  ctx.partition.k = input.p_graph->k();
-  ctx.parallel.num_threads = num_threads;
-  ctx.setup(*input.graph);
+  {
+    auto input = load_partitioned_graph(graph_filename, partition_filename);
+    ctx.partition.k = input.p_graph->k();
+    ctx.parallel.num_threads = num_threads;
+    ctx.setup(*input.graph);
 
-  std::cout << "Running refinement algorithm ..." << std::endl;
+    std::cout << "Running refinement algorithm ..." << std::endl;
 
-  START_TIMER("Benchmark");
-  START_TIMER("Allocation");
-  auto refiner = factory::create_refiner(ctx);
-  STOP_TIMER();
-  START_TIMER("Initialize");
-  refiner->initialize(*input.p_graph);
-  STOP_TIMER();
-  START_TIMER("Refinement");
-  refiner->refine(*input.p_graph, ctx.partition);
-  STOP_TIMER();
-  STOP_TIMER();
+    START_TIMER("Benchmark");
+    START_TIMER("Allocation");
+    auto refiner = factory::create_refiner(ctx);
+    STOP_TIMER();
+    START_TIMER("Initialize");
+    refiner->initialize(*input.p_graph);
+    STOP_TIMER();
+    START_TIMER("Refinement");
+    refiner->refine(*input.p_graph, ctx.partition);
+    STOP_TIMER();
+    STOP_TIMER();
 
-  std::cout << "RESULT cut=" << metrics::edge_cut(*input.p_graph)
-            << " imbalance=" << metrics::imbalance(*input.p_graph) << std::endl;
+    std::cout << "RESULT cut=" << metrics::edge_cut(*input.p_graph)
+              << " imbalance=" << metrics::imbalance(*input.p_graph) << std::endl;
+  }
 
   STOP_TIMER(); // Stop root timer
   Timer::global().print_human_readable(std::cout);
