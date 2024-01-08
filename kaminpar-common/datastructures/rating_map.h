53f8263fef65918b3b0d3dea8eb79c1c3e09faae
Daniel Seemaier <daniel@seemaier.de>
2024-01-08 16:29:08 +0100

    [dKaMinPar] Switch to BlockRatingMap during refinement

--- kaminpar-common/datastructures/rating_map.h
+++ kaminpar-common/datastructures/rating_map.h
@@ -13,6 +13,88 @@
 
 namespace kaminpar {
 template <typename Value, typename Key, typename LargeMap = FastResetArray<Value, Key>>
+class BlockRatingMap {
+  using SmallMap = FixedSizeSparseMap<Key, Value>;
+
+public:
+  enum class MapType {
+    SUPER_SMALL,
+    SMALL,
+    LARGE
+  };
+
+  explicit BlockRatingMap(const std::size_t max_size)
+      : _super_small_map(128),
+        _max_size(max_size) {}
+
+  MapType update_upper_bound_size(const std::size_t upper_bound_size) {
+    select_map(upper_bound_size);
+    return _selected_map;
+  }
+
+  template <typename F1, typename F2> decltype(auto) run_with_map(F1 &&f1, F2 &&f2) {
+    switch (_selected_map) {
+    case MapType::SUPER_SMALL:
+      return f1(_super_small_map);
+    case MapType::SMALL:
+      return f1(_small_map);
+    case MapType::LARGE:
+      return f2(_large_map);
+    }
+    __builtin_unreachable();
+  }
+
+  [[nodiscard]] std::size_t small_map_counter() const {
+    return _small_map_counter;
+  }
+
+  [[nodiscard]] std::size_t super_small_map_counter() const {
+    return _super_small_map_counter;
+  }
+
+  [[nodiscard]] std::size_t large_map_counter() const {
+    return _large_map_counter;
+  }
+
+  [[nodiscard]] std::size_t max_size() const {
+    return _max_size;
+  }
+
+  void change_max_size(const std::size_t max_size) {
+    _max_size = max_size;
+  }
+
+private:
+  void select_map(const std::size_t upper_bound_size) {
+    if (upper_bound_size < 128) {
+      _selected_map = MapType::SUPER_SMALL;
+      ++_super_small_map_counter;
+    } else if (_max_size < SmallMap::MAP_SIZE || upper_bound_size > SmallMap::MAP_SIZE / 3) {
+      _selected_map = MapType::LARGE;
+      ++_large_map_counter;
+    } else {
+      _selected_map = MapType::SMALL;
+      ++_small_map_counter;
+    }
+
+    if (_selected_map == MapType::LARGE && _large_map.capacity() < _max_size) {
+      _large_map.resize(_max_size);
+    }
+  }
+
+  std::size_t _max_size;
+
+  MapType _selected_map = MapType::SMALL;
+  FastResetArray<Value, Key> _super_small_map;
+  SmallMap _small_map{};
+  LargeMap _large_map{}; // allocate on demand
+
+  std::size_t _super_small_map_counter = 0;
+  std::size_t _small_map_counter = 0;
+  std::size_t _large_map_counter = 0;
+};
+
+template <typename Value, typename Key, typename LargeMap = FastResetArray<Value, Key>>
 class RatingMap {
   using SuperSmallMap = FixedSizeSparseMap<Key, Value, 128>;
   using SmallMap = FixedSizeSparseMap<Key, Value>;
