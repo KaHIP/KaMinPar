cd738baa42a94b2af177be5543610e35be894430
Daniel Seemaier <daniel+github@seemaier.de>
2024-04-29 11:42:00 +0200

    refactor: remove unused declarations

--- kaminpar-common/datastructures/scalable_vector.h
+++ kaminpar-common/datastructures/scalable_vector.h
@@ -2,8 +2,6 @@
 
 #include <tbb/scalable_allocator.h>
 
-#include "kaminpar-common/datastructures/noinit_vector.h"
-
 namespace kaminpar {
 #ifdef KAMINPAR_ENABLE_HEAP_PROFILING
 // @deprecated
@@ -13,16 +11,5 @@ template <typename T> using scalable_vector = std::vector<T>;
 template <typename T> using scalable_vector = std::vector<T, tbb::scalable_allocator<T>>;
 #endif
 
-#ifdef KAMINPAR_ENABLE_HEAP_PROFILING
-// @deprecated
-template <typename T> using scalable_noinit_vector = std::vector<T, NoinitAllocator<T>>;
-#else
-// @deprecated
-template <typename T>
-using scalable_noinit_vector = std::vector<T, NoinitAllocator<T, tbb::scalable_allocator<T>>>;
-#endif
-
 template <typename T> using ScalableVector = scalable_vector<T>;
-
-template <typename T> using ScalableNoinitVector = scalable_noinit_vector<T>;
 } // namespace kaminpar
