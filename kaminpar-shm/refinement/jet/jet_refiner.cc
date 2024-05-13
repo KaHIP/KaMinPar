e540c50aa6274c79ff33142cf90e322ecb4f6a75
Daniel Seemaier <daniel+github@seemaier.de>
2024-05-13 13:38:31 +0200

    feat: make shm jet more like dist jet

--- kaminpar-shm/refinement/jet/jet_refiner.cc
+++ kaminpar-shm/refinement/jet/jet_refiner.cc
@@ -8,14 +8,11 @@
  ******************************************************************************/
 #include "kaminpar-shm/refinement/jet/jet_refiner.h"
 
-#include "kaminpar-shm/datastructures/delta_partitioned_graph.h"
 #include "kaminpar-shm/datastructures/partitioned_graph.h"
 #include "kaminpar-shm/metrics.h"
 #include "kaminpar-shm/refinement/balancer/greedy_balancer.h"
-#include "kaminpar-shm/refinement/gains/sparse_gain_cache.h"
+#include "kaminpar-shm/refinement/gains/dense_gain_cache.h"
 
-#include "kaminpar-common/datastructures/noinit_vector.h"
-#include "kaminpar-common/degree_buckets.h"
 #include "kaminpar-common/logger.h"
 #include "kaminpar-common/timer.h"
 
@@ -29,23 +26,32 @@ void JetRefiner::initialize(const PartitionedGraph &p_graph) {
   SCOPED_TIMER("Jet Refiner");
   SCOPED_TIMER("Initialization");
 
-  _negative_gain_factor = (p_graph.n() == _ctx.partition.n)
-                              ? _ctx.refinement.jet.fine_negative_gain_factor
-                              : _ctx.refinement.jet.coarse_negative_gain_factor;
-  DBG << "Initialized with negative gain factor: " << _negative_gain_factor;
+  const bool is_coarse_level = p_graph.n() < _ctx.partition.n;
+  if (is_coarse_level) {
+    _num_rounds = _ctx.refinement.jet.num_rounds_on_coarse_level;
+    _initial_gain_temp = _ctx.refinement.jet.initial_gain_temp_on_coarse_level;
+    _final_gain_temp = _ctx.refinement.jet.final_gain_temp_on_coarse_level;
+  } else {
+    _num_rounds = _ctx.refinement.jet.num_rounds_on_fine_level;
+    _initial_gain_temp = _ctx.refinement.jet.initial_gain_temp_on_fine_level;
+    _final_gain_temp = _ctx.refinement.jet.final_gain_temp_on_fine_level;
+  }
+
+  DBG << "Initialized Jet refiner: " << _num_rounds << " rounds, gain temperature in range ["
+      << _initial_gain_temp << ", " << _final_gain_temp << "]";
 }
 
 bool JetRefiner::refine(PartitionedGraph &p_graph, const PartitionContext &p_ctx) {
   SCOPED_TIMER("Jet Refiner");
 
   START_TIMER("Allocation");
-  SparseGainCache<true> gain_cache(_ctx, p_graph.n(), p_graph.k());
+  DenseGainCache<> gain_cache(_ctx, p_graph.n(), p_graph.k());
   gain_cache.initialize(p_graph);
 
-  NoinitVector<BlockID> next_partition(p_graph.n());
+  StaticArray<BlockID> next_partition(p_graph.n());
   p_graph.pfor_nodes([&](const NodeID u) { next_partition[u] = 0; });
 
-  NoinitVector<std::uint8_t> lock(p_graph.n());
+  StaticArray<std::uint8_t> lock(p_graph.n());
   p_graph.pfor_nodes([&](const NodeID u) { lock[u] = 0; });
 
   GreedyBalancer balancer(_ctx);
@@ -68,135 +74,155 @@ bool JetRefiner::refine(PartitionedGraph &p_graph, const PartitionContext &p_ctx
   DBG << "Running JET refinement for at most " << max_num_iterations << " iterations and at most "
       << max_num_fruitless_iterations << " fruitless iterations";
 
-  int cur_fruitless_iteration = 0;
-  int cur_iteration = 0;
+  for (int round = 0; round < _num_rounds; ++round) {
+    int cur_fruitless_iteration = 0;
+    int cur_iteration = 0;
 
-  do {
-    TIMED_SCOPE("Find moves") {
-      p_graph.pfor_nodes([&](const NodeID u) {
-        const BlockID from = p_graph.block(u);
+    const double gain_temp = compute_gain_temp(round);
+    LOG << "Round " << (round + 1) << " of " << _num_rounds << ": use gain temperature "
+        << gain_temp;
 
-        if (lock[u] || !gain_cache.is_border_node(u, from)) {
-          next_partition[u] = from;
-          return;
-        }
+    do {
+      TIMED_SCOPE("Find moves") {
+        p_graph.pfor_nodes([&](const NodeID u) {
+          const BlockID from = p_graph.block(u);
 
-        EdgeWeight best_gain = std::numeric_limits<EdgeWeight>::min();
-        BlockID best_block = from;
-
-        for (const BlockID to : p_graph.blocks()) {
-          if (to == from) {
-            continue;
+          if (lock[u] || !gain_cache.is_border_node(u, from)) {
+            next_partition[u] = from;
+            return;
           }
 
-          const EdgeWeight gain = gain_cache.gain(u, from, to);
-          if (gain > best_gain) {
-            best_gain = gain;
-            best_block = to;
-          }
-        }
+          EdgeWeight best_gain = std::numeric_limits<EdgeWeight>::min();
+          BlockID best_block = from;
 
-        if (best_gain > -std::floor(_negative_gain_factor * gain_cache.conn(u, from))) {
-          next_partition[u] = best_block;
-        } else {
-          next_partition[u] = from;
-        }
-      });
-    };
-
-    TIMED_SCOPE("Filter moves") {
-      p_graph.pfor_nodes([&](const NodeID u) {
-        lock[u] = 0;
-
-        const BlockID from = p_graph.block(u);
-        const BlockID to = next_partition[u];
-        if (from == to) {
-          return;
-        }
-
-        const EdgeWeight gain_u = gain_cache.gain(u, from, to);
-        EdgeWeight gain = 0;
-
-        for (const auto &[e, v] : p_graph.neighbors(u)) {
-          const EdgeWeight weight = p_graph.edge_weight(e);
-
-          const bool v_before_u = [&, v = v] {
-            const BlockID from_v = p_graph.block(v);
-            const BlockID to_v = next_partition[v];
-            if (from_v != to_v) {
-              const EdgeWeight gain_v = gain_cache.gain(v, from_v, to_v);
-              return gain_v > gain_u || (gain_v == gain_u && v < u);
+          for (const BlockID to : p_graph.blocks()) {
+            if (to == from) {
+              continue;
             }
-            return false;
-          }();
-          const BlockID block_v = v_before_u ? next_partition[v] : p_graph.block(v);
 
-          if (to == block_v) {
-            gain += weight;
-          } else if (from == block_v) {
-            gain -= weight;
+            const EdgeWeight gain = gain_cache.gain(u, from, to);
+            if (gain > best_gain) {
+              best_gain = gain;
+              best_block = to;
+            }
           }
-        }
 
-        if (gain > 0) {
-          lock[u] = 1;
-        }
-      });
-    };
+          if (best_gain > -std::floor(gain_temp * gain_cache.conn(u, from))) {
+            next_partition[u] = best_block;
+          } else {
+            next_partition[u] = from;
+          }
+        });
+      };
 
-    TIMED_SCOPE("Execute moves") {
-      p_graph.pfor_nodes([&](const NodeID u) {
-        if (lock[u]) {
+      TIMED_SCOPE("Filter moves") {
+        p_graph.pfor_nodes([&](const NodeID u) {
+          lock[u] = 0;
+
           const BlockID from = p_graph.block(u);
           const BlockID to = next_partition[u];
+          if (from == to) {
+            return;
+          }
 
-          p_graph.set_block(u, to);
-          gain_cache.move(p_graph, u, from, to);
+          const EdgeWeight gain_u = gain_cache.gain(u, from, to);
+          EdgeWeight gain = 0;
+
+          for (const auto &[e, v] : p_graph.neighbors(u)) {
+            const EdgeWeight weight = p_graph.edge_weight(e);
+
+            const bool v_before_u = [&, v = v] {
+              const BlockID from_v = p_graph.block(v);
+              const BlockID to_v = next_partition[v];
+              if (from_v != to_v) {
+                const EdgeWeight gain_v = gain_cache.gain(v, from_v, to_v);
+                return gain_v > gain_u || (gain_v == gain_u && v < u);
+              }
+              return false;
+            }();
+            const BlockID block_v = v_before_u ? next_partition[v] : p_graph.block(v);
+
+            if (to == block_v) {
+              gain += weight;
+            } else if (from == block_v) {
+              gain -= weight;
+            }
+          }
+
+          if (gain > 0) {
+            lock[u] = 1;
+          }
+        });
+      };
+
+      TIMED_SCOPE("Execute moves") {
+        p_graph.pfor_nodes([&](const NodeID u) {
+          if (lock[u]) {
+            const BlockID from = p_graph.block(u);
+            const BlockID to = next_partition[u];
+
+            p_graph.set_block(u, to);
+            gain_cache.move(p_graph, u, from, to);
+          }
+        });
+      };
+
+      TIMED_SCOPE("Rebalance") {
+        balancer.refine(p_graph, p_ctx);
+      };
+
+      const EdgeWeight final_cut = metrics::edge_cut(p_graph);
+
+      TIMED_SCOPE("Update best partition") {
+        if (final_cut <= best_cut) {
+          p_graph.pfor_nodes([&](const NodeID u) { best_partition[u] = p_graph.block(u); });
+          last_iteration_is_best = true;
+        } else {
+          last_iteration_is_best = false;
         }
-      });
-    };
+      };
 
-    TIMED_SCOPE("Rebalance") {
-      balancer.refine(p_graph, p_ctx);
-    };
+      ++cur_iteration;
+      ++cur_fruitless_iteration;
 
-    const EdgeWeight final_cut = metrics::edge_cut(p_graph);
-
-    TIMED_SCOPE("Update best partition") {
-      if (final_cut <= best_cut) {
-        p_graph.pfor_nodes([&](const NodeID u) { best_partition[u] = p_graph.block(u); });
-        last_iteration_is_best = true;
+      if (best_cut - final_cut > (1.0 - _ctx.refinement.jet.fruitless_threshold) * best_cut) {
+        DBG << "Improved cut from " << initial_cut << " to " << best_cut << " to " << final_cut
+            << ": resetting number of fruitless iterations (threshold: "
+            << _ctx.refinement.jet.fruitless_threshold << ")";
+        cur_fruitless_iteration = 0;
       } else {
-        last_iteration_is_best = false;
+        DBG << "Fruitless edge cut change from " << initial_cut << " to " << best_cut << " to "
+            << final_cut << " (threshold: " << _ctx.refinement.jet.fruitless_threshold
+            << "): incrementing fruitless iterations counter to " << cur_fruitless_iteration;
+      }
+
+      if (final_cut < best_cut) {
+        best_cut = final_cut;
+      }
+    } while (cur_iteration < max_num_iterations &&
+             cur_fruitless_iteration < max_num_fruitless_iterations);
+
+    TIMED_SCOPE("Rollback") {
+      if (!last_iteration_is_best) {
+        p_graph.pfor_nodes([&](const NodeID u) { p_graph.set_block(u, best_partition[u]); });
+
+        // Re-initialize gain cache and balancer after rollback
+        gain_cache.initialize(p_graph);
+        balancer.initialize(p_graph);
+        balancer.track_moves(&gain_cache);
       }
     };
+  }
 
-    ++cur_iteration;
-    ++cur_fruitless_iteration;
+  return false; // (ignored)
+}
 
-    if (best_cut - final_cut > (1.0 - _ctx.refinement.jet.fruitless_threshold) * best_cut) {
-      DBG << "Improved cut from " << initial_cut << " to " << best_cut << " to " << final_cut
-          << ": resetting number of fruitless iterations (threshold: "
-          << _ctx.refinement.jet.fruitless_threshold << ")";
-      cur_fruitless_iteration = 0;
-    } else {
-      DBG << "Fruitless edge cut change from " << initial_cut << " to " << best_cut << " to "
-          << final_cut << " (threshold: " << _ctx.refinement.jet.fruitless_threshold
-          << "): incrementing fruitless iterations counter to " << cur_fruitless_iteration;
-    }
+double JetRefiner::compute_gain_temp(int round) const {
+  if (_num_rounds > 1) {
+    const double alpha = 1.0 * round / (_num_rounds - 1);
+    return _initial_gain_temp + alpha * (_final_gain_temp - _initial_gain_temp);
+  }
 
-    if (final_cut < best_cut) {
-      best_cut = final_cut;
-    }
-  } while (cur_iteration < max_num_iterations &&
-           cur_fruitless_iteration < max_num_fruitless_iterations);
-
-  TIMED_SCOPE("Rollback") {
-    if (!last_iteration_is_best) {
-      p_graph.pfor_nodes([&](const NodeID u) { p_graph.set_block(u, best_partition[u]); });
-    }
-  };
-
-  return false;
+  return 1.0 * (_initial_gain_temp + _final_gain_temp) / 2;
 }
 } // namespace kaminpar::shm
