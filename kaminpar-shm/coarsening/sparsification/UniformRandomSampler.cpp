4a754f266f96facd5af20567710868ece677eaf1
Dominik Rosch <dominik.rosch@student.kit.edu>
2024-05-13 11:08:36 +0200

    remove unnessary counters in random sampler

--- kaminpar-shm/coarsening/sparsification/UniformRandomSampler.cpp
+++ kaminpar-shm/coarsening/sparsification/UniformRandomSampler.cpp
@@ -9,17 +9,12 @@
 namespace kaminpar::shm::sparsification {
 
 StaticArray<EdgeWeight> UniformRandomSampler::sample(const CSRGraph &g) {
-  unsigned int frontedges = 0;
   StaticArray<EdgeWeight> sample = StaticArray<EdgeWeight>(g.m());
-  unsigned int edges_deleted = 0;
   for (int v = 0; v < g.n(); ++v) {
     for (EdgeID e : g.incident_edges(v)) {
       KASSERT(v != g.edge_target(e), "no loops allowed", assert::always);
       if (v < g.edge_target(e)) {
-        frontedges++;
         sample[e] = Random::instance().random_bool(_probability) ? g.edge_weight(e) : 0;
-        if (!sample[e])
-          edges_deleted++;
       }
     }
   }
