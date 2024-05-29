bba68ff36a34a063360d6c7b22de4db3a3426582
Dominik Rosch <dominik.rosch@student.kit.edu>
2024-05-29 10:22:30 +0200

    fix julia and extract data back into C++

--- kaminpar-shm/coarsening/sparsification/EffectiveResistanceSampler.cpp
+++ kaminpar-shm/coarsening/sparsification/EffectiveResistanceSampler.cpp
@@ -15,36 +15,89 @@ EffectiveResistanceSampler::~EffectiveResistanceSampler() {
   jl_atexit_hook(0);
 }
 
-struct IJV {
-  EdgeID *i;
-  EdgeID *j;
-  double *v;
-  NodeID n;
-};
+void EffectiveResistanceSampler::print_jl_exception() {
+  jl_value_t *exception = jl_exception_occurred();
+  jl_value_t *sprint_fun = jl_get_function(jl_main_module, "sprint");
+  jl_value_t *showerror_fun = jl_get_function(jl_main_module, "showerror");
 
-StaticArray<EdgeWeight>
-EffectiveResistanceSampler::sample(const CSRGraph &g, EdgeID target_edge_amount) {
+  JL_GC_PUSH3(&exception, &sprint_fun, &showerror_fun);
+
+  if (exception) {
+    const char *returned_exception = jl_string_ptr(jl_call2(sprint_fun, showerror_fun, exception));
+    printf("ERROR: %s\n", returned_exception);
+  }
+
+  jl_exception_clear();
+  JL_GC_POP();
+}
+EffectiveResistanceSampler::IJV *EffectiveResistanceSampler::encode_as_ijv(const CSRGraph &g) {
   // Encode ajacency matrix in csc fromat: A[I[n],J[n]] = V[n] and all other entries are zero
   IJV *a = (IJV *)malloc(sizeof(IJV));
-  a->i = (EdgeID *)malloc(sizeof(EdgeID) * g.m());
-  a->j = (EdgeID *)malloc(sizeof(EdgeID) * g.m()), a->v = (double *)malloc(sizeof(double) * g.m());
+  a->i = (int64_t *)malloc(sizeof(int64_t) * g.m());
+  a->j = (int64_t *)malloc(sizeof(int64_t) * g.m());
+  a->v = (double *)malloc(sizeof(double) * g.m());
   for (NodeID source : g.nodes()) {
     for (EdgeID edge : g.incident_edges(source)) {
       NodeID target = g.edge_target(edge);
-      a->i[edge] = source;
-      a->j[edge] = target;
+      a->i[edge] = source + 1;
+      a->j[edge] = target + 1;
       a->v[edge] = g.edge_weight(edge);
     }
   }
+  a->m = g.m();
+  return a;
+}
 
+StaticArray<EdgeWeight>
+EffectiveResistanceSampler::extract_sample(const CSRGraph &g, IJV *sparsifyer) {
+  auto sampled_edges = StaticArray<std::tuple<NodeID, NodeID, EdgeWeight>>(sparsifyer->m);
+  for (size_t k = 0; k < sparsifyer->m; k++) {
+    sampled_edges[k] = std::make_tuple(
+        sparsifyer->i[k] - 1, sparsifyer->j[k] - 1, static_cast<EdgeWeight>(sparsifyer->v[k])
+    );
+  }
+  std::sort(sampled_edges.begin(), sampled_edges.end(), [&](const auto &a, const auto &b) {
+    return std::get<0>(a) < std::get<0>(b) ||
+           (std::get<0>(a) == std::get<0>(b) && std::get<1>(a) < std::get<1>(b));
+  });
+  auto sample = StaticArray<EdgeWeight>(g.m(), 0);
+
+  auto sorted_by_target_permutation = StaticArray<EdgeID>(g.m());
+  for (EdgeID e : g.edges())
+    sorted_by_target_permutation[e] = e;
+  for (NodeID u : g.nodes()) {
+    std::sort(
+        sorted_by_target_permutation.begin() + g.raw_nodes()[u],
+        sorted_by_target_permutation.begin() + g.raw_nodes()[u + 1],
+        [&](const EdgeID e1, const EdgeID e2) { return g.edge_target(e1) <= g.edge_target(e2); }
+    );
+  }
+  size_t k = 0;
+  for (NodeID u : g.nodes()) {
+    if (k == sparsifyer->m)
+      break;
+    for (EdgeID i : g.incident_edges(u)) {
+      EdgeID e = sorted_by_target_permutation[i];
+      NodeID v = g.edge_target(e);
+      auto [x, y, w] = sampled_edges[k];
+      if (u == x && v == y) {
+        sample[e] = w;
+        k++;
+      }
+    }
+  }
+  KASSERT(k == sparsifyer->m, "Not alle sampled edges were added to sample!", assert::always);
+  return sample;
+}
+EffectiveResistanceSampler::IJV *EffectiveResistanceSampler::sparsify_in_julia(IJV *a) {
   jl_eval_string(R"(
   module Adapter
     using Laplacians
     using SparseArrays
 
     struct C_IJV
-        i::Array{Cint,1}
-        j::Array{Cint,1}
+        i::Array{Int64,1}
+        j::Array{Int64,1}
         v::Array{Cdouble,1}
     end
 
@@ -53,40 +106,69 @@ EffectiveResistanceSampler::sample(const CSRGraph &g, EdgeID target_edge_amount)
         (i, j, v) = findnz(sparsifyed)
         return C_IJV(i, j, v)
     end
+
+    function get_i(A::C_IJV)
+        return A.i
+    end
+    function get_j(A::C_IJV)
+        return A.j
+    end
+    function get_v(A::C_IJV)
+        return A.v
+    end
+    function get_m(A::C_IJV)
+        return length(A.i)
+    end
+
+    function get_first(arr)
+      return arr[1]
+    end
   end
   )");
 
-  jl_value_t *jl_int_array_type = jl_apply_array_type((jl_value_t *)jl_int32_type, 1);
-  jl_array_t *jl_I = jl_ptr_to_array_1d(jl_int_array_type, a->i, g.m(), 1);
-  jl_array_t *jl_J = jl_ptr_to_array_1d(jl_int_array_type, a->j, g.m(), 1);
+  // TODO: Enable GC carefully
+  JL_GC_DISABLED(1);
+
+  jl_value_t *jl_int_array_type = jl_apply_array_type((jl_value_t *)jl_int64_type, 1);
+  jl_array_t *jl_I = jl_ptr_to_array_1d(jl_int_array_type, a->i, a->m, 0);
+  jl_array_t *jl_J = jl_ptr_to_array_1d(jl_int_array_type, a->j, a->m, 0);
   jl_array_t *jl_V =
-      jl_ptr_to_array_1d(jl_apply_array_type((jl_value_t *)jl_float64_type, 1), a->v, g.m(), 1);
+      jl_ptr_to_array_1d(jl_apply_array_type((jl_value_t *)jl_float64_type, 1), a->v, a->m, 0);
 
   jl_module_t *adapter = (jl_module_t *)jl_eval_string("Adapter");
-  /*
-  jl_value_t *jl_a = jl_new_struct(
-      (jl_datatype_t *)jl_get_function(adapter, "C_IJV"), jl_I, jl_J, jl_V
-  );
-  */
-  jl_function_t *jl_ijv = jl_get_function(adapter, "C_IJV");
-  jl_value_t *jl_a = jl_call3(jl_ijv, (jl_value_t *)jl_I, (jl_value_t *)jl_J, (jl_value_t *)jl_V);
+
+  jl_value_t *jl_a =
+      jl_new_struct((jl_datatype_t *)jl_get_function(adapter, "C_IJV"), jl_I, jl_J, jl_V);
+
+  // TODO: Pick eps in a sensible way
+  jl_value_t *jl_eps = jl_box_float32(0.1);
+
   jl_function_t *sparsify_adapter = jl_get_function(adapter, "sparsify_adapter");
-  KASSERT(
-      sparsify_adapter != nullptr,
-      "Could not find function sparsify_adapter in Julia.",
-      assert::always
-  );
+  jl_value_t *jl_sparsifyer = jl_call2(sparsify_adapter, jl_a, jl_eps);
+  print_jl_exception();
 
-  jl_value_t *jl_eps = jl_box_float32(0.7);
+  KASSERT(jl_sparsifyer != nullptr, "sparsify_adapter failed!", assert::always);
 
-  KASSERT(jl_is_structtype(jl_a), "jl_a is not a struct", assert::always);
+  IJV *sparsifyer = (IJV *)malloc(sizeof(IJV));
+  sparsifyer->i =
+      (int64_t *)jl_array_data(jl_call1(jl_get_function(adapter, "get_i"), jl_sparsifyer));
+  sparsifyer->j =
+      (int64_t *)jl_array_data(jl_call1(jl_get_function(adapter, "get_j"), jl_sparsifyer));
+  sparsifyer->v =
+      (double *)jl_array_data(jl_call1(jl_get_function(adapter, "get_v"), jl_sparsifyer));
+  sparsifyer->m = jl_unbox_uint32(jl_call1(jl_get_function(adapter, "get_m"), jl_sparsifyer));
 
-  jl_value_t *jl_sparsifier = jl_call2(sparsify_adapter, jl_a, jl_eps);
+  print_jl_exception();
+  return sparsifyer;
+}
 
-  if (jl_exception_occurred())
-    printf(
-        "%s: %s \n", jl_typeof_str(jl_exception_occurred()), jl_typeof_str(jl_current_exception())
-    );
-  return StaticArray<EdgeWeight>(g.m());
+StaticArray<EdgeWeight>
+EffectiveResistanceSampler::sample(const CSRGraph &g, EdgeID target_edge_amount) {
+  // TODO: Free memory and maybe reduce use of C-style pointer
+  IJV *a = encode_as_ijv(g);
+
+  IJV * sparsifyer = sparsify_in_julia(a);
+
+  return extract_sample(g, sparsifyer);
 }
 } // namespace kaminpar::shm::sparsification
--- kaminpar-shm/coarsening/sparsification/EffectiveResistanceSampler.h
+++ kaminpar-shm/coarsening/sparsification/EffectiveResistanceSampler.h
@@ -5,7 +5,8 @@
 #pragma once
 
 #include <julia.h>
-// JULIA_DEFINE_FAST_TLS // only define this once, in an executable (not in a shared library) if you want fast code.
+// JULIA_DEFINE_FAST_TLS // only define this once, in an executable (not in a shared library) if you
+// want fast code.
 
 #include "Sampler.h"
 
@@ -18,5 +19,17 @@ public:
   EffectiveResistanceSampler();
   ~EffectiveResistanceSampler();
   StaticArray<EdgeWeight> sample(const CSRGraph &g, EdgeID target_edge_amount) override;
+
+private:
+  struct IJV {
+    int64_t *i;
+    int64_t *j;
+    double *v;
+    EdgeID m;
+  };
+  void print_jl_exception();
+  IJV *encode_as_ijv(const CSRGraph &g);
+  StaticArray<EdgeWeight> extract_sample(const CSRGraph &g, IJV *sparsifyer);
+  IJV *sparsify_in_julia(IJV *a);
 };
 } // namespace kaminpar::shm::sparsification
