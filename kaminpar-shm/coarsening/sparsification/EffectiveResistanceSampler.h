a314fb7958eba04bceec32ead3672aea51d958b6
Dominik Rosch <dominik.rosch@student.kit.edu>
2024-05-31 19:07:34 +0200

    enable garbage colllector and free memory

--- kaminpar-shm/coarsening/sparsification/EffectiveResistanceSampler.h
+++ kaminpar-shm/coarsening/sparsification/EffectiveResistanceSampler.h
@@ -5,8 +5,6 @@
 #pragma once
 
 #include <julia.h>
-// JULIA_DEFINE_FAST_TLS // only define this once, in an executable (not in a shared library) if you
-// want fast code.
 
 #include "Sampler.h"
 
@@ -27,9 +25,47 @@ private:
     double *v;
     EdgeID m;
   };
+  IJV alloc_ijv(EdgeID m);
+  void free_ijv(IJV &a);
   void print_jl_exception();
-  IJV *encode_as_ijv(const CSRGraph &g);
-  StaticArray<EdgeWeight> extract_sample(const CSRGraph &g, IJV *sparsifyer);
-  IJV *sparsify_in_julia(IJV *a);
+  IJV encode_as_ijv(const CSRGraph &g);
+  StaticArray<EdgeWeight> extract_sample(const CSRGraph &g, IJV &sparsifyer);
+  IJV sparsify_in_julia(IJV &a);
+
+  inline static const char *JL_LAPLACIANS_ADAPTER_CODE = R"(
+  module LapaciansAdapter
+    using Laplacians
+    using SparseArrays
+
+    struct C_IJV
+        i::Array{Int64,1}
+        j::Array{Int64,1}
+        v::Array{Cdouble,1}
+    end
+
+    function sparsify_adapter(A::C_IJV, eps::Cfloat)::C_IJV
+        sparsifyed = sparsify(sparse(A.i, A.j, A.v),ep = eps)
+        (i, j, v) = findnz(sparsifyed)
+        return C_IJV(i, j, v)
+    end
+
+    function get_i(A::C_IJV)
+        return A.i
+    end
+    function get_j(A::C_IJV)
+        return A.j
+    end
+    function get_v(A::C_IJV)
+        return A.v
+    end
+    function get_m(A::C_IJV)
+        return length(A.i)
+    end
+
+    function get_first(arr)
+      return arr[1]
+    end
+  end
+  )";
 };
 } // namespace kaminpar::shm::sparsification
