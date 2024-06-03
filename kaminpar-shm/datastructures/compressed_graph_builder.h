55f12b6be8735351d852abcbb2001897b38b5542
Daniel Salwasser <danielsalwater@gmail.com>
2024-06-03 18:03:11 +0200

    refactor(compressed-graph): deduplicate parallel compression code

--- kaminpar-shm/datastructures/compressed_graph_builder.h
+++ kaminpar-shm/datastructures/compressed_graph_builder.h
@@ -9,7 +9,11 @@
 
 #include "kaminpar-shm/datastructures/compressed_graph.h"
 
+#include "kaminpar-common/datastructures/concurrent_circular_vector.h"
+#include "kaminpar-common/timer.h"
+
 namespace kaminpar::shm {
+SET_DEBUG(false);
 
 class CompressedEdgesBuilder {
   using NodeID = CompressedGraph::NodeID;
@@ -229,6 +233,43 @@ class ParallelCompressedGraphBuilder {
 
 public:
   /*!
+   * Compresses a graph.
+   *
+   * @param num_nodes The number of nodes of the graph to compress.
+   * @param num_edges The number of edges of the graph to compress.
+   * @param has_node_weights Whether node weights are stored.
+   * @param has_edge_weights Whether edge weights are stored.
+   * @param sorted Whether the nodes are stored in degree-bucket order.
+   * @param node_mapper Function that maps old node IDs to (possibly) new ones.
+   * @param degrees Function that returns the degree of a (remapped) node.
+   * @param nodes Function that returns the first edge of a node.
+   * @param edges Function that returns the (remapped) adjacent node of an edge.
+   * @param node_weights Function that returns the weight of a node.
+   * @param edge_weights Function that returns the weight of an edge.
+   * @return The compressed graph.
+   */
+  template <
+      typename PermutationMapper,
+      typename DegreeMapper,
+      typename NodeMapper,
+      typename EdgeMapper,
+      typename NodeWeightMapper,
+      typename EdgeWeightMapper>
+  [[nodiscard]] static CompressedGraph compress(
+      const NodeID num_nodes,
+      const EdgeID num_edges,
+      const bool has_node_weights,
+      const bool has_edge_weights,
+      const bool sorted,
+      const PermutationMapper &&node_mapper,
+      const DegreeMapper &&degrees,
+      const NodeMapper &&nodes,
+      const EdgeMapper &&edges,
+      const NodeWeightMapper &&node_weights,
+      const EdgeWeightMapper &&edge_weights
+  );
+
+  /*!
    * Compresses a graph stored in compressed sparse row format.
    *
    * @param graph The graph to compress.
@@ -257,8 +298,8 @@ public:
    * Adds a node to the compressed graph.
    *
    * @param node The node to add.
-   * @param offset The offset into the compressed edge array at which the compressed neighborhood of
-   * the node is stored.
+   * @param offset The offset into the compressed edge array at which the compressed neighborhood
+   * of the node is stored.
    */
   void add_node(const NodeID node, const EdgeID offset);
 
@@ -301,8 +342,8 @@ public:
   );
 
   /*!
-   * Finalizes the compressed graph. Note that all nodes, compressed neighborhoods, node weights and
-   * edge weights have to be added at this point.
+   * Finalizes the compressed graph. Note that all nodes, compressed neighborhoods, node weights
+   * and edge weights have to be added at this point.
    *
    * @return The resulting compressed graph.
    */
@@ -330,5 +371,247 @@ private:
   std::size_t _num_interval_nodes;
   std::size_t _num_intervals;
 };
+
+namespace debug {
+
+using Duration = std::chrono::high_resolution_clock::duration;
+
+struct Stats {
+  Duration compression_time{0};
+  Duration sync_time{0};
+  Duration copy_time{0};
+
+  std::size_t num_chunks{0};
+  std::size_t num_edges{0};
+};
+
+template <typename Lambda> decltype(auto) scoped_time(auto &elapsed, Lambda &&l) {
+  constexpr bool kNonReturning = std::is_void_v<std::invoke_result_t<Lambda>>;
+
+  if constexpr (kDebug) {
+    if constexpr (kNonReturning) {
+      auto start = std::chrono::high_resolution_clock::now();
+      l();
+      auto end = std::chrono::high_resolution_clock::now();
+      elapsed += end - start;
+    } else {
+      auto start = std::chrono::high_resolution_clock::now();
+      decltype(auto) val = l();
+      auto end = std::chrono::high_resolution_clock::now();
+      elapsed += end - start;
+      return val;
+    }
+  } else {
+    return l();
+  }
+}
+
+void print_stats(const auto &stats_ets) {
+  DBG << "Chunk distribution:";
+
+  std::size_t cur_thread = 0;
+  for (const auto &stats : stats_ets) {
+    DBG << "t" << ++cur_thread << ": " << stats.num_chunks;
+  }
+
+  DBG << "Edge distribution:";
+
+  cur_thread = 0;
+  for (const auto &stats : stats_ets) {
+    DBG << "t" << ++cur_thread << ": " << stats.num_edges;
+  }
+
+  DBG << "Time distribution: (compression, sync, copy) [s]";
+
+  const auto to_sec = [&](auto elapsed) {
+    return std::chrono::duration_cast<std::chrono::milliseconds>(elapsed).count() / 1000.0;
+  };
+
+  Duration total_time_compression(0);
+  Duration total_time_sync(0);
+  Duration total_time_copy(0);
+
+  cur_thread = 0;
+  for (const auto &stats : stats_ets) {
+    total_time_compression += stats.compression_time;
+    total_time_sync += stats.sync_time;
+    total_time_copy += stats.copy_time;
+
+    DBG << "t" << ++cur_thread << ": " << to_sec(stats.compression_time) << ' '
+        << to_sec(stats.sync_time) << ' ' << to_sec(stats.copy_time);
+  }
+
+  DBG << "sum: " << to_sec(total_time_compression) << ' ' << to_sec(total_time_sync) << ' '
+      << to_sec(total_time_copy);
+}
+
+} // namespace debug
+
+template <
+    typename PermutationMapper,
+    typename DegreeMapper,
+    typename NodeMapper,
+    typename EdgeMapper,
+    typename NodeWeightMapper,
+    typename EdgeWeightMapper>
+CompressedGraph ParallelCompressedGraphBuilder::compress(
+    const NodeID num_nodes,
+    const EdgeID num_edges,
+    const bool has_node_weights,
+    const bool has_edge_weights,
+    const bool sorted,
+    const PermutationMapper &&node_mapper,
+    const DegreeMapper &&degrees,
+    const NodeMapper &&nodes,
+    const EdgeMapper &&edges,
+    const NodeWeightMapper &&node_weights,
+    const EdgeWeightMapper &&edge_weights
+) {
+  // To compress the graph in parallel the nodes are split into chunks. Each parallel task fetches
+  // a chunk and compresses the neighbourhoods of the corresponding nodes. The compressed
+  // neighborhoods are meanwhile stored in a buffer. They are moved into the compressed edge array
+  // when the (total) length of the compressed neighborhoods of the previous chunks is determined.
+  constexpr std::size_t kNumChunks = 5000;
+  const EdgeID max_chunk_size = num_edges / kNumChunks;
+  std::vector<std::tuple<NodeID, NodeID, EdgeID>> chunks;
+
+  NodeID max_degree = 0;
+  TIMED_SCOPE("Compute chunks") {
+    NodeID cur_chunk_start = 0;
+    EdgeID cur_chunk_size = 0;
+    EdgeID cur_first_edge = 0;
+    for (NodeID i = 0; i < num_nodes; ++i) {
+      NodeID node = node_mapper(i);
+
+      const NodeID degree = degrees(node);
+      max_degree = std::max(max_degree, degree);
+
+      cur_chunk_size += degree;
+      if (cur_chunk_size >= max_chunk_size) {
+        if (cur_chunk_start == i) {
+          chunks.emplace_back(cur_chunk_start, i + 1, cur_first_edge);
+
+          cur_chunk_start = i + 1;
+          cur_first_edge += degree;
+          cur_chunk_size = 0;
+        } else {
+          chunks.emplace_back(cur_chunk_start, i, cur_first_edge);
+
+          cur_chunk_start = i;
+          cur_first_edge += cur_chunk_size - degree;
+          cur_chunk_size = degree;
+        }
+      }
+    }
+
+    if (cur_chunk_start != num_nodes) {
+      chunks.emplace_back(cur_chunk_start, num_nodes, cur_first_edge);
+    }
+  };
+
+  // Initializes the data structures used to build the compressed graph in parallel.
+  ParallelCompressedGraphBuilder builder(
+      num_nodes, num_edges, has_node_weights, has_edge_weights, sorted
+  );
+
+  tbb::enumerable_thread_specific<std::vector<EdgeID>> offsets_ets;
+  tbb::enumerable_thread_specific<std::vector<std::pair<NodeID, EdgeWeight>>> neighbourhood_ets;
+  tbb::enumerable_thread_specific<CompressedEdgesBuilder> neighbourhood_builder_ets([&] {
+    return CompressedEdgesBuilder(
+        num_nodes, num_edges, max_degree, has_edge_weights, builder.edge_weights()
+    );
+  });
+
+  const std::size_t num_threads = tbb::this_task_arena::max_concurrency();
+  ConcurrentCircularVectorMutex<NodeID, EdgeID> buffer(num_threads);
+
+  tbb::enumerable_thread_specific<debug::Stats> dbg_ets;
+  tbb::parallel_for<NodeID>(0, chunks.size(), [&](const auto) {
+    auto &dbg = dbg_ets.local();
+    IF_DBG dbg.num_chunks++;
+
+    std::vector<EdgeID> &offsets = offsets_ets.local();
+    std::vector<std::pair<NodeID, EdgeWeight>> &neighbourhood = neighbourhood_ets.local();
+    CompressedEdgesBuilder &neighbourhood_builder = neighbourhood_builder_ets.local();
+
+    const NodeID chunk = buffer.next();
+    const auto [start, end, first_edge] = chunks[chunk];
+
+    NodeWeight local_node_weight = 0;
+    neighbourhood_builder.init(first_edge);
+
+    // Compress the neighborhoods of the nodes in the fetched chunk.
+    debug::scoped_time(dbg.compression_time, [&] {
+      for (NodeID i = start; i < end; ++i) {
+        const NodeID node = node_mapper(i);
+        const NodeID degree = degrees(node);
+        IF_DBG dbg.num_edges += degree;
+
+        EdgeID edge = nodes(node);
+        for (NodeID j = 0; j < degree; ++j) {
+          const NodeID adjacent_node = edges(edge);
+
+          EdgeWeight edge_weight;
+          if (has_edge_weights) [[unlikely]] {
+            edge_weight = edge_weights(edge);
+          } else {
+            edge_weight = 1;
+          }
+
+          neighbourhood.emplace_back(adjacent_node, edge_weight);
+          edge += 1;
+        }
+
+        const EdgeID local_offset = neighbourhood_builder.add(i, neighbourhood);
+        offsets.push_back(local_offset);
+
+        neighbourhood.clear();
+      }
+    });
+
+    // Wait for the parallel tasks that process the previous chunks to finish.
+    const EdgeID offset = debug::scoped_time(dbg.sync_time, [&] {
+      const EdgeID compressed_neighborhoods_size = neighbourhood_builder.size();
+      return buffer.fetch_and_update(chunk, compressed_neighborhoods_size);
+    });
+
+    // Store the edge offset and node weight for each node in the chunk and copy the compressed
+    // neighborhoods into the actual compressed edge array.
+    debug::scoped_time(dbg.copy_time, [&] {
+      for (NodeID i = start; i < end; ++i) {
+        const EdgeID local_offset = offsets[i - start];
+
+        builder.add_node(i, offset + local_offset);
+
+        if (has_node_weights) [[unlikely]] {
+          const NodeID node = node_mapper(i);
+          const NodeWeight node_weight = node_weights(node);
+          local_node_weight += node_weight;
+
+          builder.add_node_weight(i, node_weight);
+        }
+      }
+      offsets.clear();
+
+      builder.add_compressed_edges(
+          offset, neighbourhood_builder.size(), neighbourhood_builder.compressed_data()
+      );
+
+      builder.record_local_statistics(
+          neighbourhood_builder.max_degree(),
+          local_node_weight,
+          neighbourhood_builder.total_edge_weight(),
+          neighbourhood_builder.num_high_degree_nodes(),
+          neighbourhood_builder.num_high_degree_parts(),
+          neighbourhood_builder.num_interval_nodes(),
+          neighbourhood_builder.num_intervals()
+      );
+    });
+  });
+
+  IF_DBG debug::print_stats(dbg_ets);
+
+  return builder.build();
+}
 
 } // namespace kaminpar::shm
