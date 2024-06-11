bb4c2a5455cc66a2f2f298e981bc4f54f4b30b23
Dominik Rosch <dominik.rosch@student.kit.edu>
2024-06-11 11:12:07 +0200

    er in julia

---
+++ kaminpar-shm/coarsening/sparsification/er.jl
@@ -0,0 +1,44 @@
+using Laplacians
+using SparseArrays
+
+struct C_IJV
+    i::Array{Int64,1}
+    j::Array{Int64,1}
+    v::Array{Cdouble,1}
+end
+
+# based on sparsify method in module Lapacians
+function effective_resistances(g::C_IJV)::C_IJV
+  a = sparse(g.i, g.j, g.v)
+  f = approxchol_lap(a,tol=1e-2);
+
+  n = size(a,1)
+  k = round(Int, JLfac*log(n)) # number of dims for Johnson-Lindenstrauss
+
+  U = wtedEdgeVertexMat(a)
+  m = size(U,1)
+  R = randn(Float64, m,k)
+  UR = U'*R;
+
+  V = zeros(n,k)
+  for i in 1:k
+    V[:,i] = f(UR[:,i])
+  end
+
+  (ai,aj,av) = findnz(triu(a))
+  ers = zeros(size(av))
+  for h in 1:length(av)
+    i = ai[h]
+    j = aj[h]
+    # divided by k, because 1/sqrt(k)
+    ers[h] = min(1,av[h]* ((norm(V[i,:]-V[j,:])^2)/k))
+    #        min(1, w_e * (R_e                      ))
+  end
+  erMatrix = sparse(ai,aj,ers)
+  erMatrix = erMatrix + erMatrix'
+
+  (i,j,v) = findnz(erMatrix)
+  return C_IJV(i,j,v)
+  return
+end
+print("hi\n")
