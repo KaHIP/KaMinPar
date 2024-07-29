727819e44dd86b28858673118ca34703164df09d
Daniel Salwasser <danielsalwater@gmail.com>
2024-07-29 18:34:59 +0200

    fix(kaminpar-shm): correctly read graphs stored in compressed format with degree bucket ordering

--- kaminpar-common/datastructures/compact_static_array.h
+++ kaminpar-common/datastructures/compact_static_array.h
@@ -141,7 +141,12 @@ public:
   /*!
    * Constructs an unitialized CompactStaticArray.
    */
-  CompactStaticArray() : _byte_width(0), _size(0), _unrestricted_size(0), _num_values(0) {
+  CompactStaticArray()
+      : _byte_width(0),
+        _size(0),
+        _num_values(0),
+        _unrestricted_size(0),
+        _unrestricted_num_values(0) {
     RECORD_DATA_STRUCT(0, _struct);
   }
 
@@ -170,11 +175,12 @@ public:
   )
       : _byte_width(byte_width),
         _size(actual_size),
-        _unrestricted_size(actual_size),
         _num_values((_size - (sizeof(Int) - _byte_width)) / _byte_width),
         _values(std::move(data)),
-        _read_mask(std::numeric_limits<Int>::max() << (byte_width * 8)),
-        _write_mask(std::numeric_limits<Int>::max() << (byte_width * 8)) {
+        _read_mask(std::numeric_limits<Int>::max() >> ((sizeof(Int) - byte_width) * 8)),
+        _write_mask(std::numeric_limits<Int>::max() << (byte_width * 8)),
+        _unrestricted_size(_size),
+        _unrestricted_num_values(_num_values) {
     RECORD_DATA_STRUCT(0, _struct);
     KASSERT(actual_size >= sizeof(Int) - _byte_width);
     KASSERT(byte_width >= 1);
@@ -199,13 +205,15 @@ public:
 
     _byte_width = byte_width;
     _size = num_values * byte_width + sizeof(Int) - byte_width;
-    _unrestricted_size = _size;
 
     _num_values = num_values;
     _values = std::make_unique<std::uint8_t[]>(_size);
 
     _read_mask = std::numeric_limits<Int>::max() >> ((sizeof(Int) - byte_width) * 8);
     _write_mask = std::numeric_limits<Int>::max() << (byte_width * 8);
+
+    _unrestricted_size = _size;
+    _unrestricted_num_values = num_values;
 
     IF_HEAP_PROFILING(_struct->size = std::max(_struct->size, _size));
   }
