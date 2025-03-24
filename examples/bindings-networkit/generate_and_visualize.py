444ccef9b822949c5ce13991e1f5770d65822095
Daniel Seemaier <daniel@seemaier.de>
2025-03-24 14:24:47 +0100

    .

--- examples/bindings-networkit/generate_and_visualize.py
+++ examples/bindings-networkit/generate_and_visualize.py
@@ -5,7 +5,7 @@ from networkit import vizbridges
 graph = nk.generators.HyperbolicGenerator(100, k = 16, gamma = 2.7, T = 0).generate()
 partition = kaminpar.KaMinPar(graph).computePartitionWithEpsilon(4, 0.03)
 
-fig = nk.vizbridges.widgetFromGraph(graph, dimension = nk.vizbridges.Dimension.Three, nodePartition = partition, nodePalette = [(0, 0, 0), (255, 0, 0)])
+fig = nk.vizbridges.widgetFromGraph(graph, dimension = nk.vizbridges.Dimension.Three, nodePartition = partition, nodePalette = [(0, 0, 0), (255, 0, 0), (0, 255, 0), (0, 0, 255)])
 fig.write_image("partitioned_hyperbolic.png")
 
 
