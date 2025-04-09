a7a80f868cd9d1bf2d9048a3fb174f6666c8dc49
Daniel Seemaier <daniel+github@seemaier.de>
2025-04-09 10:41:28 +0200

    refactor: speedup wff

--- kaminpar-shm/coarsening/sparsification/weighted_forest_fire_score.cc
+++ kaminpar-shm/coarsening/sparsification/weighted_forest_fire_score.cc
@@ -3,89 +3,89 @@
 #include <queue>
 #include <unordered_set>
 
-#include "sparsification_utils.h"
+#include "kaminpar-shm/coarsening/sparsification/sparsification_utils.h"
 
+#include "kaminpar-common/datastructures/fast_reset_array.h"
 #include "kaminpar-common/random.h"
 #include "kaminpar-common/timer.h"
 
 namespace kaminpar::shm::sparsification {
 StaticArray<EdgeID> WeightedForestFireScore::scores(const CSRGraph &g) {
-  StaticArray<EdgeID> burnt(g.m(), 0);
+  StaticArray<EdgeID> burnt(g.m());
+
   std::atomic<EdgeID> edges_burnt = 0;
   std::atomic<int> number_of_fires = 0;
 
   tbb::parallel_for(0, tbb::this_task_arena::max_concurrency(), [&](auto) {
-    // Preallocate everything here
     std::queue<NodeID> activeNodes;
-    std::unordered_set<EdgeID> visited;
     std::vector<std::pair<EdgeID, double>> validEdgesWithKeys;
+    FastResetArray<bool> visited(g.n());
+    Random &rand = Random::instance();
 
     while (edges_burnt < _targetBurnRatio * g.m()) {
       number_of_fires++;
 
       // Start a new fire
+      activeNodes.push(rand.random_index(0, g.n()));
       visited.clear();
-      activeNodes.push(Random::instance().random_index(0, g.n()));
-      visited.insert(activeNodes.front());
+      visited[activeNodes.front()] = true;
 
       EdgeID localEdgesBurnt = 0;
 
       while (!activeNodes.empty()) {
         const NodeID u = activeNodes.front();
         activeNodes.pop();
-
         validEdgesWithKeys.clear();
 
         // We sample neighbors without replacement and probailies propotional to the weight of the
         // connecting edge using a version of the exponential clock method: Every incident edge
         // leading to an unvisited vertex is assined a random key depending on its weight and the
         // ones with the smallest keys are sampled.
-        for (EdgeID e : g.incident_edges(u)) {
-          if (!visited.contains(g.edge_target(e))) {
+        g.neighbors(u, [&](const EdgeID e, const NodeID v) {
+          if (!visited.get(v)) {
+            // Key for exponetial clock method
             validEdgesWithKeys.emplace_back(
-                e,
-                // key for exponetial clock method
-                -std::log(Random::instance().random_double()) /
-                    (_ignore_weights ? 1 : g.edge_weight(e))
+                e, -std::log(rand.random_double()) / (_ignore_weights ? 1 : g.edge_weight(e))
             );
           }
-        }
+        });
+
+        // Shifted geometric distribution
         const EdgeID neighbours_to_sample = std::min(
-            static_cast<EdgeID>(
-                std::ceil(std::log(Random::instance().random_double()) / std::log(_pf)) - 1
-            ), // shifted geometric distribution
+            static_cast<EdgeID>(std::ceil(std::log(rand.random_double()) / std::log(_pf)) - 1),
             static_cast<EdgeID>(validEdgesWithKeys.size())
         );
         KASSERT(neighbours_to_sample <= validEdgesWithKeys.size());
-        if (neighbours_to_sample == 0)
-          continue;
 
-        auto end_of_samping_range = validEdgesWithKeys.begin() + neighbours_to_sample;
+        if (neighbours_to_sample == 0) {
+          continue;
+        }
+
+        const auto end_of_samping_range = validEdgesWithKeys.begin() + neighbours_to_sample;
         std::nth_element(
             validEdgesWithKeys.begin(),
             end_of_samping_range - 1,
             validEdgesWithKeys.end(),
-            [](auto a, auto b) {
-              return std::get<1>(a) < std::get<1>(b);
-            } // comp keys
+            [](auto a, auto b) { return std::get<1>(a) < std::get<1>(b); }
         );
 
         for (auto p = validEdgesWithKeys.begin(); p != end_of_samping_range; ++p) {
-          // mark NodeID as visited, burn edge
-          auto [e, _] = *p;
-          NodeID v = g.edge_target(e);
+          // Mark node as visited, burn edge
+          const EdgeID e = p->first;
+          const NodeID v = g.edge_target(e);
+
           activeNodes.push(v);
           __atomic_add_fetch(&burnt[e], 1, __ATOMIC_RELAXED);
           localEdgesBurnt++;
-          visited.insert(v);
+          visited[v] = true;
         }
       }
+
       edges_burnt += localEdgesBurnt;
     }
   });
 
   print_fire_statistics(g, edges_burnt, number_of_fires);
-
   make_scores_symmetric(g, burnt);
 
   // Not normalized unlike in the NetworKit implementation
