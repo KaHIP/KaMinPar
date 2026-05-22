b77c35f8822364d09489f8134ae7d558b218b369
ufsay <ufsay@student.kti.edu>
2026-05-22 16:41:29 +0200

    .

--- kaminpar-shm/refinement/lhop/lhop_refiner.cc
+++ kaminpar-shm/refinement/lhop/lhop_refiner.cc
@@ -405,7 +405,7 @@ bool LHopRefiner::refine(PartitionedGraph &p_graph, const PartitionContext &p_ct
       break;
     case 4: //Move all - partitionwise
       LOG << "START ALL-BATCH 10";
-      for(int i = 0; i < 10; i++) {
+      for(int i = 0; i < 20; i++) {
         if(partitionGains.empty()) {
           break;
         }
--- kaminpar-shm/refinement/lhop/lhop_refiner.h
+++ kaminpar-shm/refinement/lhop/lhop_refiner.h
@@ -56,12 +56,11 @@ private:
 
   const CSRGraph *_graph = nullptr;
 
-  //is lHop 2-3 better than edge-cut?
   const std::size_t l = 2; // SET L used for LHop -> TODO replace
 
   const std::vector<int> lweights = {1, 1, 1};
 
-  const int batchtype = 0;
+  const int batchtype = 2;
 
   void initializeLHopModel(PartitionedGraph &p_graph, std::vector<std::vector<LHopTable>> &lhopModel);
 
