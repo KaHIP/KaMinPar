a3f5f74b62a394f0e70d366ab843dd85beb4feec
ufsay <ufsay@student.kti.edu>
2026-06-19 18:35:18 +0200

    .

--- kaminpar-shm/refinement/lhop/lhop_refiner.h
+++ kaminpar-shm/refinement/lhop/lhop_refiner.h
@@ -56,7 +56,7 @@ private:
 
   const CSRGraph *_graph = nullptr;
 
-  const std::size_t l = 4; // SET L used for LHop -> TODO replace
+  const std::size_t l = 1; // SET L used for LHop -> TODO replace
 
   const std::vector<int> lweights = {1, 1, 1, 1, 1};
 
