0a04c64b2ed0e4350a734146e019d51f395bf6fb
ufsay <ufsay@student.kti.edu>
2026-06-22 10:55:40 +0200

    .

--- kaminpar-shm/refinement/lhop/lhop_refiner.h
+++ kaminpar-shm/refinement/lhop/lhop_refiner.h
@@ -60,7 +60,7 @@ private:
 
   const std::vector<int> lweights = {1, 1, 1, 1, 1};
 
-  const int batchtype = 3;
+  const int batchtype = 2;
 
   void initializeLHopModel(PartitionedGraph &p_graph, std::vector<std::vector<LHopTable>> &lhopModel);
 
