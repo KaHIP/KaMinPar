922e1ea8c6c05cc1b4fbb4b93d3c7914eb15da8e
ufsay <ufsay@student.kti.edu>
2026-06-23 16:29:26 +0200

    ...

--- kaminpar-shm/refinement/lhop/lhop_refiner.cc
+++ kaminpar-shm/refinement/lhop/lhop_refiner.cc
@@ -320,7 +320,7 @@ bool LHopRefiner::refine(PartitionedGraph &p_graph, const PartitionContext &p_ct
   std::vector<std::vector<LHopTable>> lhopModel(p_graph.n());
   initializeLHopModel(p_graph, lhopModel);
   std::vector<int> nodeCycleWeight(p_graph.n(), 0);
-  calculateCycles(p_graph, nodeCycleWeight);
+  //calculateCycles(p_graph, nodeCycleWeight);
 
 
   LOG << "Calculate Gains";
@@ -417,6 +417,8 @@ bool LHopRefiner::refine(PartitionedGraph &p_graph, const PartitionContext &p_ct
       break;
 
   }
+  lhopModel.clear();
+  initializeLHopModel(p_graph, lhopModel);
   nodeGains.clear();
   partitionGains.clear();
   endSum = calculateGains(p_graph, lhopModel, nodeGains, partitionGains, nodeCycleWeight);
